f86570da1f223ac714509f2610ee9f185f1e19ef
Rebecca Turner <me@re-becca.org>
2018-08-22 12:55:22 -0700

    gen-changelog: Actually match content on fixes lines

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -90,7 +90,7 @@ function main () {
     } else if (m = line.match(/^Credit: @(.*)/)) {
       if (!commit.credit) commit.credit = []
       commit.credit.push(m[1])
-    } else if (m = line.match(/^Fixes: #?(.*?)/)) {
+    } else if (m = line.match(/^Fixes: #?(.*)/)) {
       commit.fixes = m[1]
     } else if (m = line.match(/^Reviewed-By: @(.*)/)) {
       commit.reviewed = m[1]
