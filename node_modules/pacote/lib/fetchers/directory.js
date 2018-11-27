cc1d111f8406113ba00634649b34ba3669b2ea50
Kat Marchán <kzm@zkat.tech>
2018-11-27 09:28:05 -0800

    libnpm@2.0.0

--- node_modules/pacote/lib/fetchers/directory.js
+++ node_modules/pacote/lib/fetchers/directory.js
@@ -34,11 +34,11 @@ Fetcher.impl(fetchDirectory, {
     const pkgPath = path.join(spec.fetchSpec, 'package.json')
     const srPath = path.join(spec.fetchSpec, 'npm-shrinkwrap.json')
     return BB.join(
-      readFileAsync(pkgPath).then(JSON.parse).catch({code: 'ENOENT'}, err => {
+      readFileAsync(pkgPath).then(JSON.parse).catch({ code: 'ENOENT' }, err => {
         err.code = 'ENOPACKAGEJSON'
         throw err
       }),
-      readFileAsync(srPath).then(JSON.parse).catch({code: 'ENOENT'}, () => null),
+      readFileAsync(srPath).then(JSON.parse).catch({ code: 'ENOENT' }, () => null),
       (pkg, sr) => {
         pkg._shrinkwrap = sr
         pkg._hasShrinkwrap = !!sr
@@ -50,7 +50,7 @@ Fetcher.impl(fetchDirectory, {
     ).then(pkg => {
       if (!pkg.bin && pkg.directories && pkg.directories.bin) {
         const dirBin = pkg.directories.bin
-        return glob(path.join(spec.fetchSpec, dirBin, '/**'), {nodir: true}).then(matches => {
+        return glob(path.join(spec.fetchSpec, dirBin, '/**'), { nodir: true }).then(matches => {
           matches.forEach(filePath => {
             const relative = path.relative(spec.fetchSpec, filePath)
             if (relative && relative[0] !== '.') {
