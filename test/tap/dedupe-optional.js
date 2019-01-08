624b131bad699c480f0986c86aad55d20837abe2
Lars Willighagen <lars.willighagen@gmail.com>
2019-01-08 19:38:57 +0100

    test: update dedupe-optional with maketest

--- test/tap/dedupe-optional.js
+++ test/tap/dedupe-optional.js
@@ -1,82 +1,96 @@
-var fs = require('graceful-fs')
-var path = require('path')
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const test = require('tap').test
+const mr = require('npm-registry-mock')
+const Tacks = require('tacks')
+const File = Tacks.File
+const Symlink = Tacks.Symlink
+const Dir = Tacks.Dir
+const common = require('../common-tap.js')
 
-var mkdirp = require('mkdirp')
-var mr = require('npm-registry-mock')
-var rimraf = require('rimraf')
-var test = require('tap').test
+const basedir = path.join(__dirname, path.basename(__filename, '.js'))
+const testdir = path.join(basedir, 'testdir')
+const cachedir = path.join(basedir, 'cache')
+const globaldir = path.join(basedir, 'global')
+const tmpdir = path.join(basedir, 'tmp')
 
-var common = require('../common-tap.js')
-var server
-
-var pkg = path.join(__dirname, 'dedupe-optional')
-
-var EXEC_OPTS = { cwd: pkg }
-
-var json = {
-  author: 'Dedupe tester',
-  name: 'dedupe',
-  version: '0.0.0',
-  dependencies: {
-    clean: '2.1.6'
-  }
+const conf = {
+  cwd: testdir,
+  env: Object.assign({}, process.env, {
+    npm_config_cache: cachedir,
+    npm_config_tmp: tmpdir,
+    npm_config_prefix: globaldir,
+    npm_config_registry: common.registry,
+    npm_config_loglevel: 'warn'
+  })
 }
 
-var shrinkwrap = {
-  name: 'dedupe',
-  version: '0.0.0',
-  dependencies: {
-    clean: {
-      version: '2.1.6'
-    }
-  }
-}
+let server
+const fixture = new Tacks(Dir({
+  cache: Dir(),
+  global: Dir(),
+  tmp: Dir(),
+  testdir: Dir({
+    'package-lock.json': File({
+      name: 'dedupe-optional',
+      version: '1.0.0',
+      lockfileVersion: 1,
+      requires: true,
+      dependencies: {
+        async: {
+          version: '0.9.2',
+          resolved: 'https://registry.npmjs.org/async/-/async-0.9.2.tgz',
+          integrity: 'sha1-rqdNXmHB+JlhO/ZL2mbUx48v0X0=',
+          optional: true
+        }
+      }
+    }),
+    'package.json': File({
+      name: 'dedupe-optional',
+      version: '1.0.0',
+      optionalDependencies: {
+        async: '*'
+      }
+    })
+  })
+}))
 
 test('setup', function (t) {
-  t.comment('test for https://npm.community/t/3807')
-  setup(function () {
-    t.end()
+  setup()
+  mr({port: common.port, throwOnUnmatched: true}, function (err, s) {
+    if (err) throw err
+    server = s
+    t.done()
   })
 })
 
 test('dedupe keeps uninstalled packages in package-lock.json', function (t) {
-  common.npm(['dedupe'], EXEC_OPTS, function (err, code) {
-    t.ifError(err, 'successfully deduped against previous install')
-    t.notOk(code, 'npm dedupe exited with code')
+  t.comment('test for https://npm.community/t/3807')
+  common.npm(['dedupe'], conf, function (err, code) {
+    if (err) throw err
+    t.is(code, 0, 'command ran ok')
 
-    var shrinkwrap = JSON.parse(fs.readFileSync('package-lock.json', 'utf8'))
+    const shrinkwrap = JSON.parse(
+      fs.readFileSync(
+        path.join(testdir, 'package-lock.json'), 'utf8'))
 
     t.ok(shrinkwrap.dependencies, 'npm dedupe kept packages')
-    t.end()
+    t.done()
   })
 })
 
 test('cleanup', function (t) {
   server.close()
   cleanup()
-
-  t.end()
+  t.done()
 })
 
 function cleanup () {
-  rimraf.sync(pkg)
+  fixture.remove(basedir)
 }
 
-function setup (cb) {
+function setup () {
   cleanup()
-  mkdirp.sync(pkg)
-  fs.writeFileSync(
-    path.join(pkg, 'package.json'),
-    JSON.stringify(json, null, 2)
-  )
-  fs.writeFileSync(
-    path.join(pkg, 'package-lock.json'),
-    JSON.stringify(shrinkwrap, null, 2)
-  )
-  process.chdir(pkg)
-
-  mr({ port: common.port }, function (er, s) {
-    server = s
-    cb()
-  })
+  fixture.create(basedir)
 }
