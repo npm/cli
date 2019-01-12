ade7a5116acf8a056cd1eb735c5981f2ac061b20
Tanohzan <tanohzana@gmail.com>
2019-01-12 19:00:33 +0100

    wip: testing deps install

--- test/tap/mp.js
+++ test/tap/mp.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 const common  = require("../common-tap.js")
 const path = require("path")
 const test = require("tap").test
@@ -6,11 +6,13 @@ const rimraf = require("rimraf")
 const basePath = path.resolve(__dirname, path.basename(__filename, ".js"))
 const fixturePath = path.resolve(basePath, "npm-test-files")
 const testDirPath = path.resolve(fixturePath, "npm-test-mp")
-const Tacks = require('tacks')
+const Tacks = require("tacks")
 const File = Tacks.File
 const Dir = Tacks.Dir
 
 const jsFile1 = "require('uninstalledDep1')"
+const jsFile2 = "require('dep1'); require('dep2')"
+const jsFile3 = "require('dep1'); require('dep3')"
 
 test("setup", function (t) {
   cleanup()
@@ -18,20 +20,18 @@ test("setup", function (t) {
 })
 
 test("mp check simple file", function(t) {
-  const fixture = new Tacks(
-    Dir({
-      "npm-test-mp": Dir({
-        "package.json": File({
-          name: "npm-test-mp",
-          version: "1.0.0",
-          dependencies: {
-            "dep1": "installedDep1"
-          }
-        }),
-        "file1.js": File(jsFile1)
-      })
+  const fixture = new Tacks(Dir({
+    "npm-test-mp": Dir({
+      "package.json": File({
+        name: "npm-test-mp",
+        version: "1.0.0",
+        dependencies: {
+          dep1: "installedDep1"
+        }
+      }),
+      "file1.js": File(jsFile1)
     })
-  )
+  }))
 
   withFixture(t, fixture, function(done) {
     common.npm(["mp", "check", "./file1.js"], {
@@ -42,6 +42,120 @@ test("mp check simple file", function(t) {
       // Removes special characters and emojis
       const message = stdout.trim().substr(3)
       t.same(message, "Package(s) to install: uninstalledDep1")
+      done()
+    })
+  })
+})
+
+test("mp c (check) simple file", function(t) {
+  const fixture = new Tacks(Dir({
+    "npm-test-mp": Dir({
+      "package.json": File({
+        name: "npm-test-mp",
+        version: "1.0.0",
+        dependencies: {
+          dep1: "installedDep1"
+        }
+      }),
+      "file1.js": File(jsFile1)
+    })
+  }))
+
+  withFixture(t, fixture, function(done) {
+    common.npm(["mp", "c", "./file1.js"], {
+      cwd: testDirPath
+    }, function(err, code, stdout, stderr) {
+      t.ifErr(err, "mp succeeded")
+      t.equal(0, code, "exit 0 on mp")
+      // Removes special characters and emojis
+      const message = stdout.trim().substr(3)
+      t.same(message, "Package(s) to install: uninstalledDep1")
+      done()
+    })
+  })
+})
+
+test("mp check folder", function(t) {
+  const fixture = new Tacks(Dir({
+    "npm-test-mp": Dir({
+      "package.json": File({
+        name: "npm-test-mp",
+        version: "1.0.0",
+        dependencies: {
+          dep1: "installedDep1"
+        }
+      }),
+      "file2.js": File(jsFile2),
+      "file3.js": File(jsFile3)
+    })
+  }))
+
+  withFixture(t, fixture, function(done) {
+    common.npm(["mp", "check", "."], {
+      cwd: testDirPath
+    }, function(err, code, stdout, stderr) {
+      t.ifErr(err, "mp succeeded")
+      t.equal(0, code, "exit 0 on mp")
+      // Removes special characters and emojis
+      const message = stdout.trim().substr(3)
+      t.same(message, "Package(s) to install: dep2,dep3")
+      done()
+    })
+  })
+})
+
+test("mp check folder but all installed", function(t) {
+  const fixture = new Tacks(Dir({
+    "npm-test-mp": Dir({
+      "package.json": File({
+        name: "npm-test-mp",
+        version: "1.0.0",
+        dependencies: {
+          dep1: "installedDep1",
+          dep2: "installedDep2",
+          dep3: "installedDep3"
+        }
+      }),
+      "file2.js": File(jsFile2),
+      "file3.js": File(jsFile3)
+    })
+  }))
+
+  withFixture(t, fixture, function(done) {
+    common.npm(["mp", "check", "."], {
+      cwd: testDirPath
+    }, function(err, code, stdout, stderr) {
+      t.ifErr(err, "mp succeeded")
+      t.equal(0, code, "exit 0 on mp")
+      // Removes special characters and emojis
+      const message = stdout.trim().substr(2)
+      t.same(message, "No package to install")
+      done()
+    })
+  })
+})
+
+test("mp install missing deps in file", function(t) {
+  const fixture = new Tacks(Dir({
+    "npm-test-mp": Dir({
+      "package.json": File({
+        name: "npm-test-mp",
+        version: "1.0.0",
+        dependencies: {
+          dep1: "installedDep1",
+        }
+      }),
+      "file1.js": File(jsFile2)
+    })
+  }))
+
+  withFixture(t, fixture, function(done) {
+    common.npm(["mp", "install", "file1.js", "-y"], {
+      cwd: testDirPath
+    }, function(err, code, stdout, stderr) {
+      t.ifErr(err, "mp succeeded")
+      t.equal(0, code, "exit 0 on mp")
+      // @TODO: verify that -y works and check packages.json deps
       done()
     })
   })
