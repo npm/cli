1e8fe84c9d73663b167d8631f11b232ba7afe196
Kyle E. Mitchell <kyle@kemitchell.com>
2019-09-10 01:01:44 -0700

    install: improve `npm support` test

--- test/tap/install-mention-support.js
+++ test/tap/install-mention-support.js
@@ -5,34 +5,35 @@ var Dir = Tacks.Dir
 var File = Tacks.File
 var common = require('../common-tap.js')
 
-var testdir = common.pkg
+var fixturepath = common.pkg
 var fixture = new Tacks(Dir({
-  'b-src': Dir({
+  'package.json': File({}),
+  'hassupport': Dir({
     'package.json': File({
-      name: 'b',
-      version: '1.0.0',
-      support: 'http://example.com/support.json'
+      name: 'hassupport',
+      version: '7.7.7',
+      support: 'http://example.com/project/support'
     })
   })
 }))
 
 test('setup', function (t) {
-  fixture.remove(testdir)
-  fixture.create(testdir)
+  fixture.remove(fixturepath)
+  fixture.create(fixturepath)
   t.end()
 })
 
 test('install-report', function (t) {
-  common.npm(['install', '--no-save', './b-src'], {cwd: testdir}, function (err, code, stdout, stderr) {
+  common.npm(['install', '--no-save', './hassupport'], {cwd: fixturepath}, function (err, code, stdout, stderr) {
     if (err) throw err
     t.is(code, 0, 'installed successfully')
     t.is(stderr, '', 'no warnings')
-    t.includes(stdout, '`npm support`', 'metions `npm support`')
+    t.includes(stdout, '`npm support`', 'mentions `npm support`')
     t.end()
   })
 })
 
 test('cleanup', function (t) {
-  fixture.remove(testdir)
+  fixture.remove(fixturepath)
   t.end()
 })
