bae18601faa9d3572b11a12d6723c33f5be8c443
claudiahdz <cghr1990@gmail.com>
2019-10-22 19:25:30 +0300

    chore(docs): fix docs url path

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
-        path: `${__dirname}/src/`,
+        path: `${__dirname}/content/`,
       },
     },
     'gatsby-transformer-sharp',
