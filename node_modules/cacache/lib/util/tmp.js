c583d1629728034ad3a39d916379a02ca9c59d7b
claudiahdz <cghr1990@gmail.com>
2020-02-06 15:25:33 -0500

    cacache@14.0.0

--- node_modules/cacache/lib/util/tmp.js
+++ node_modules/cacache/lib/util/tmp.js
@@ -1,18 +1,20 @@
 'use strict'
 
-const BB = require('bluebird')
+const util = require('util')
 
 const figgyPudding = require('figgy-pudding')
 const fixOwner = require('./fix-owner')
 const path = require('path')
-const rimraf = BB.promisify(require('rimraf'))
+const rimraf = util.promisify(require('rimraf'))
 const uniqueFilename = require('unique-filename')
+const { disposer } = require('./disposer')
 
 const TmpOpts = figgyPudding({
   tmpPrefix: {}
 })
 
 module.exports.mkdir = mktmpdir
+
 function mktmpdir (cache, opts) {
   opts = TmpOpts(opts)
   const tmpTarget = uniqueFilename(path.join(cache, 'tmp'), opts.tmpPrefix)
@@ -22,16 +24,19 @@ function mktmpdir (cache, opts) {
 }
 
 module.exports.withTmp = withTmp
+
 function withTmp (cache, opts, cb) {
   if (!cb) {
     cb = opts
     opts = null
   }
   opts = TmpOpts(opts)
-  return BB.using(mktmpdir(cache, opts).disposer(rimraf), cb)
+
+  return disposer(mktmpdir(cache, opts), rimraf, cb)
 }
 
 module.exports.fix = fixtmpdir
+
 function fixtmpdir (cache) {
   return fixOwner(cache, path.join(cache, 'tmp'))
 }
--- node_modules/cacache/put.js
+++ node_modules/cacache/put.js
@@ -4,7 +4,9 @@ const figgyPudding = require('figgy-pudding')
 const index = require('./lib/entry-index')
 const memo = require('./lib/memoization')
 const write = require('./lib/content/write')
-const to = require('mississippi').to
+const Flush = require('minipass-flush')
+const { PassThrough } = require('minipass-collect')
+const Pipeline = require('minipass-pipeline')
 
 const PutOpts = figgyPudding({
   algorithms: {
@@ -23,64 +25,70 @@ const PutOpts = figgyPudding({
 })
 
 module.exports = putData
+
 function putData (cache, key, data, opts) {
   opts = PutOpts(opts)
-  return write(cache, data, opts).then(res => {
-    return index.insert(
-      cache, key, res.integrity, opts.concat({ size: res.size })
-    ).then(entry => {
-      if (opts.memoize) {
-        memo.put(cache, entry, data, opts)
-      }
-      return res.integrity
-    })
+  return write(cache, data, opts).then((res) => {
+    return index
+      .insert(cache, key, res.integrity, opts.concat({ size: res.size }))
+      .then((entry) => {
+        if (opts.memoize) {
+          memo.put(cache, entry, data, opts)
+        }
+        return res.integrity
+      })
   })
 }
 
 module.exports.stream = putStream
+
 function putStream (cache, key, opts) {
   opts = PutOpts(opts)
   let integrity
   let size
-  const contentStream = write.stream(
-    cache, opts
-  ).on('integrity', int => {
-    integrity = int
-  }).on('size', s => {
-    size = s
-  })
+
   let memoData
-  let memoTotal = 0
-  const stream = to((chunk, enc, cb) => {
-    contentStream.write(chunk, enc, () => {
-      if (opts.memoize) {
-        if (!memoData) { memoData = [] }
-        memoData.push(chunk)
-        memoTotal += chunk.length
-      }
-      cb()
+  const pipeline = new Pipeline()
+  // first item in the pipeline is the memoizer, because we need
+  // that to end first and get the collected data.
+  if (opts.memoize) {
+    const memoizer = new PassThrough().on('collect', data => {
+      memoData = data
     })
-  }, cb => {
-    contentStream.end(() => {
-      index.insert(cache, key, integrity, opts.concat({ size })).then(entry => {
-        if (opts.memoize) {
-          memo.put(cache, entry, Buffer.concat(memoData, memoTotal), opts)
-        }
-        stream.emit('integrity', integrity)
-        cb()
-      })
+    pipeline.push(memoizer)
+  }
+
+  // contentStream is a write-only, not a passthrough
+  // no data comes out of it.
+  const contentStream = write.stream(cache, opts)
+    .on('integrity', (int) => {
+      integrity = int
     })
-  })
-  let erred = false
-  stream.once('error', err => {
-    if (erred) { return }
-    erred = true
-    contentStream.emit('error', err)
-  })
-  contentStream.once('error', err => {
-    if (erred) { return }
-    erred = true
-    stream.emit('error', err)
-  })
-  return stream
+    .on('size', (s) => {
+      size = s
+    })
+
+  pipeline.push(contentStream)
+
+  // last but not least, we write the index and emit hash and size,
+  // and memoize if we're doing that
+  pipeline.push(new Flush({
+    flush () {
+      return index
+        .insert(cache, key, integrity, opts.concat({ size }))
+        .then((entry) => {
+          if (opts.memoize && memoData) {
+            memo.put(cache, entry, memoData, opts)
+          }
+          if (integrity) {
+            pipeline.emit('integrity', integrity)
+          }
+          if (size) {
+            pipeline.emit('size', size)
+          }
+        })
+    }
+  }))
+
+  return pipeline
 }
