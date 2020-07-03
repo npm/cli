a3ca30abd69533bb94aba5c315b6a9c5964c5292
claudiahdz <cghr1990@gmail.com>
2020-07-03 17:56:39 -0500

    test: clean prune tests

---
+++ test/lib/prune.js
@@ -0,0 +1,27 @@
+const { test } = require('tap')
+const requireInject = require('require-inject')
+
+test('should prune using Arborist', (t) => {
+  const prune = requireInject('../../lib/prune.js', {
+    '../../lib/npm.js': {
+      prefix: 'foo',
+      flatOptions: {
+        'foo': 'bar'
+      }
+    },
+    '@npmcli/arborist': function (args) {
+      t.ok(args, 'gets options object')
+      t.ok(args.path, 'gets path option')
+      this.prune = () => {
+        t.ok(true, 'prune is called')
+      }
+    },
+    '../../lib/utils/reify-output.js': (arb) => {
+      t.ok(arb, 'gets arborist tree')
+    }
+  })
+  prune(null, () => {
+    t.ok(true, 'callback is called')
+    t.end()
+  })
+})
