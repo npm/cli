08b95ae96565197ccdcc7239ebad01474e407956
isaacs <i@izs.me>
2020-07-10 12:22:37 -0700

    module for dereferencing aliases to canonical cmd names

---
+++ test/lib/utils/deref-command.js
@@ -0,0 +1,8 @@
+const t = require('tap')
+const deref = require('../../../lib/utils/deref-command.js')
+
+t.equal(deref(null), '')
+t.equal(deref('it'), 'install-test')
+t.equal(deref('installTe'), 'install-test')
+t.equal(deref('birthday'), 'birthday')
+t.equal(deref('birt'), '')
