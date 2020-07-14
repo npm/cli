2864483b1f5a8381f5c681f4743148664d7c6faa
isaacs <i@izs.me>
2020-07-14 13:02:31 -0700

    protect against non-strings when dereferencing commands

--- test/lib/utils/deref-command.js
+++ test/lib/utils/deref-command.js
@@ -2,6 +2,7 @@ const t = require('tap')
 const deref = require('../../../lib/utils/deref-command.js')
 
 t.equal(deref(null), '')
+t.equal(deref(8), '')
 t.equal(deref('it'), 'install-test')
 t.equal(deref('installTe'), 'install-test')
 t.equal(deref('birthday'), 'birthday')
