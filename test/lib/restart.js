b513d04cd063cb776fad3d77d356da7f2c55f5cb
isaacs <i@izs.me>
2020-08-03 22:56:34 -0700

    refactor and lint commands, make consistent
    Still pending test coverage for most of these, but wanted to give them a
    clean sweep to get the "load-all-commands" tests passing.
    
    The following changes are in here:
    
    - All commands now have a `completion()` method and a usage string that
      uses the same `usage` util consistently.
    - The `silent` argument to many commands has been removed.
    - All commands use the `cmd = ${cmd}(args).then(() => cb()).catch(cb)`
      pattern consistently.
    
    Full test coverage for all commands is still lacking, and will have to
    be done prior to the GA v7 release.

--- test/lib/restart.js
+++ test/lib/restart.js
@@ -1,5 +1,5 @@
 const t = require('tap')
 const restart = require('../../lib/restart.js')
 t.isa(restart, Function)
-t.equal(restart.completion, undefined, 'no completion')
+t.equal(restart.completion, require('../../lib/utils/completion/none.js'), 'empty completion')
 t.equal(restart.usage, 'npm restart [-- <args>]')
