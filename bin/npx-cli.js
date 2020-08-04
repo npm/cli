ghmoore/cli-2

npx: add install prompt, handle options correctly
- handle previous npx options that are still possible to be handled, and
  print a warning if any deprecated/removed options are used.
- expand shorthands properly in npx command line.
- take existing npm options into account when determining placement of
  the -- argument.
- document changes from previous versions of npx.

bin/npx-cli.js
@@ -6,6 +6,58 @@ const cli = require('../lib/cli.js')
 process.argv[1] = require.resolve('./npm-cli.js')
 process.argv.splice(2, 0, 'exec')
 
+// TODO: remove the affordances for removed items in npm v9
+const removedSwitches = new Set([
+  'always-spawn',
+  'ignore-existing',
+  'shell-auto-feedback'
+])
+
+const removedOpts = new Set([
+  'npm',
+  'node-arg',
+  'n'
+])
+
+const removed = new Set([
+  ...removedSwitches,
+  ...removedOpts
+])
+
+const { types, shorthands } = require('../lib/config/defaults.js')
+const npmSwitches = Object.entries(types)
+  .filter(([key, type]) => type === Boolean ||
+    (Array.isArray(type) && type.includes(Boolean)))
+  .map(([key, type]) => key)
+
+// things that don't take a value
+const switches = new Set([
+  ...removedSwitches,
+  ...npmSwitches,
+  'no-install',
+  'quiet',
+  'q',
+  'version',
+  'v',
+  'help',
+  'h'
+])
+
+// things that do take a value
+const opts = new Set([
+  ...removedOpts,
+  'package',
+  'p',
+  'cache',
+  'userconfig',
+  'call',
+  'c',
+  'shell',
+  'npm',
+  'node-arg',
+  'n'
+])
+
 // break out of loop when we find a positional argument or --
 // If we find a positional arg, we shove -- in front of it, and
 // let the normal npm cli handle the rest.
@@ -15,11 +67,51 @@ for (i = 3; i < process.argv.length; i++) {
   if (arg === '--') {
     break
   } else if (/^-/.test(arg)) {
-    // `--package foo` treated the same as `--package=foo`
-    if (!arg.includes('=')) {
-      i++
+    const [key, ...v] = arg.replace(/^-+/, '').split('=')
+
+    switch (key) {
+      case 'p':
+        process.argv[i] = ['--package', ...v].join('=')
+        break
+
+      case 'shell':
+        process.argv[i] = ['--script-shell', ...v].join('=')
+        break
+
+      case 'no-install':
+        process.argv[i] = '--yes=false'
+        break
+
+      default:
+        // resolve shorthands and run again
+        if (shorthands[key] && !removed.has(key)) {
+          const a = [...shorthands[key]]
+          if (v.length) {
+            a.push(v.join('='))
+          }
+          process.argv.splice(i, 1, ...a)
+          i--
+          continue
+        }
+        break
     }
-    continue
+
+    if (removed.has(key)) {
+      console.error(`npx: the --${key} argument has been removed.`)
+      process.argv.splice(i, 1)
+      i--
+    }
+
+    if (v.length === 0 && !switches.has(key) &&
+        (opts.has(key) || !/^-/.test(process.argv[i + 1]))) {
+      // value will be next argument, skip over it.
+      if (removed.has(key)) {
+        // also remove the value for the cut key.
+        process.argv.splice(i + 1, 1)
+      } else {
+        i++
+      }
+    }
   } else {
     // found a positional arg, put -- in front of it, and we're done
     process.argv.splice(i, 0, '--')

test/bin/npx-cli.js
@@ -4,6 +4,14 @@ const npx = require.resolve('../../bin/npx-cli.js')
 const cli = require.resolve('../../lib/cli.js')
 const npm = require.resolve('../../bin/npm-cli.js')
 
+const logs = []
+console.error = (...msg) => logs.push(msg)
+
+t.afterEach(cb => {
+  logs.length = 0
+  cb()
+})
+
 t.test('npx foo -> npm exec -- foo', t => {
   process.argv = ['node', npx, 'foo']
   requireInject(npx, { [cli]: () => {} })
@@ -25,9 +33,62 @@ t.test('npx -x y foo -z -> npm exec -x y -- foo -z', t => {
   t.end()
 })
 
-t.test('npx --x=y foo -z -> npm exec --x=y -- foo -z', t => {
-  process.argv = ['node', npx, '--x=y', 'foo', '-z']
+t.test('npx --x=y --no-install foo -z -> npm exec --x=y -- foo -z', t => {
+  process.argv = ['node', npx, '--x=y', '--no-install', 'foo', '-z']
   requireInject(npx, { [cli]: () => {} })
-  t.strictSame(process.argv, ['node', npm, 'exec', '--x=y', '--', 'foo', '-z'])
+  t.strictSame(process.argv, ['node', npm, 'exec', '--x=y', '--yes=false', '--', 'foo', '-z'])
+  t.end()
+})
+
+t.test('transform renamed options into proper values', t => {
+  process.argv = ['node', npx, '-y', '--shell=bash', '-p', 'foo', '-c', 'asdf']
+  requireInject(npx, { [cli]: () => {} })
+  t.strictSame(process.argv, ['node', npm, 'exec', '--yes', '--script-shell=bash', '--package', 'foo', '--call', 'asdf'])
+  t.end()
+})
+
+// warn if deprecated switches/options are used
+t.test('use a bunch of deprecated switches and options', t => {
+  process.argv = [
+    'node',
+    npx,
+    '--npm',
+    '/some/npm/bin',
+    '--node-arg=--harmony',
+    '-n',
+    '--require=foobar',
+    '--reg=http://localhost:12345/',
+    '-p',
+    'foo',
+    '--always-spawn',
+    '--shell-auto-feedback',
+    '--ignore-existing',
+    '-q',
+    'foobar'
+  ]
+
+  const expect = [
+    'node',
+    npm,
+    'exec',
+    '--registry',
+    'http://localhost:12345/',
+    '--package',
+    'foo',
+    '--loglevel',
+    'warn',
+    '--',
+    'foobar'
+  ]
+  requireInject(npx, { [cli]: () => {} })
+  t.strictSame(process.argv, expect)
+  t.strictSame(logs, [
+    [ 'npx: the --npm argument has been removed.' ],
+    [ 'npx: the --node-arg argument has been removed.' ],
+    [ 'npx: the --n argument has been removed.' ],
+    [ 'npx: the --always-spawn argument has been removed.' ],
+    [ 'npx: the --shell-auto-feedback argument has been removed.' ],
+    [ 'npx: the --ignore-existing argument has been removed.' ]
+  ])
   t.end()
 })

npm/cli

bin/npx-cli.js
@@ -6,25 +6,123 @@ const cli = require('../lib/cli.js')
 process.argv[1] = require.resolve('./npm-cli.js')
 process.argv.splice(2, 0, 'exec')
 
+// TODO: remove the affordances for removed items in npm v9
+const removedSwitches = new Set([
+  'always-spawn',
+  'ignore-existing',
+  'shell-auto-fallback'
+])
+
+const removedOpts = new Set([
+  'npm',
+  'node-arg',
+  'n'
+])
+
+const removed = new Set([
+  ...removedSwitches,
+  ...removedOpts
+])
+
+const { types, shorthands } = require('../lib/config/defaults.js')
+const npmSwitches = Object.entries(types)
+  .filter(([key, type]) => type === Boolean ||
+    (Array.isArray(type) && type.includes(Boolean)))
+  .map(([key, type]) => key)
+
+// things that don't take a value
+const switches = new Set([
+  ...removedSwitches,
+  ...npmSwitches,
+  'no-install',
+  'quiet',
+  'q',
+  'version',
+  'v',
+  'help',
+  'h'
+])
+
+// things that do take a value
+const opts = new Set([
+  ...removedOpts,
+  'package',
+  'p',
+  'cache',
+  'userconfig',
+  'call',
+  'c',
+  'shell',
+  'npm',
+  'node-arg',
+  'n'
+])
+
 // break out of loop when we find a positional argument or --
 // If we find a positional arg, we shove -- in front of it, and
 // let the normal npm cli handle the rest.
 let i
+let sawRemovedFlags = false
 for (i = 3; i < process.argv.length; i++) {
   const arg = process.argv[i]
   if (arg === '--') {
     break
   } else if (/^-/.test(arg)) {
-    // `--package foo` treated the same as `--package=foo`
-    if (!arg.includes('=')) {
-      i++
+    const [key, ...v] = arg.replace(/^-+/, '').split('=')
+
+    switch (key) {
+      case 'p':
+        process.argv[i] = ['--package', ...v].join('=')
+        break
+
+      case 'shell':
+        process.argv[i] = ['--script-shell', ...v].join('=')
+        break
+
+      case 'no-install':
+        process.argv[i] = '--yes=false'
+        break
+
+      default:
+        // resolve shorthands and run again
+        if (shorthands[key] && !removed.has(key)) {
+          const a = [...shorthands[key]]
+          if (v.length) {
+            a.push(v.join('='))
+          }
+          process.argv.splice(i, 1, ...a)
+          i--
+          continue
+        }
+        break
     }
-    continue
+
+    if (removed.has(key)) {
+      console.error(`npx: the --${key} argument has been removed.`)
+      sawRemovedFlags = true
+      process.argv.splice(i, 1)
+      i--
+    }
+
+    if (v.length === 0 && !switches.has(key) &&
+        (opts.has(key) || !/^-/.test(process.argv[i + 1]))) {
+      // value will be next argument, skip over it.
+      if (removed.has(key)) {
+        // also remove the value for the cut key.
+        process.argv.splice(i + 1, 1)
+      } else {
+        i++
+      }
+    }
   } else {
     // found a positional arg, put -- in front of it, and we're done
     process.argv.splice(i, 0, '--')
     break
   }
+}
+
+if (sawRemovedFlags) {
+  console.error('See `npm help exec` for more information')
 }
 
 cli(process)

test/bin/npx-cli.js
@@ -4,6 +4,14 @@ const npx = require.resolve('../../bin/npx-cli.js')
 const cli = require.resolve('../../lib/cli.js')
 const npm = require.resolve('../../bin/npm-cli.js')
 
+const logs = []
+console.error = (...msg) => logs.push(msg)
+
+t.afterEach(cb => {
+  logs.length = 0
+  cb()
+})
+
 t.test('npx foo -> npm exec -- foo', t => {
   process.argv = ['node', npx, 'foo']
   requireInject(npx, { [cli]: () => {} })
@@ -25,9 +33,63 @@ t.test('npx -x y foo -z -> npm exec -x y -- foo -z', t => {
   t.end()
 })
 
-t.test('npx --x=y foo -z -> npm exec --x=y -- foo -z', t => {
-  process.argv = ['node', npx, '--x=y', 'foo', '-z']
+t.test('npx --x=y --no-install foo -z -> npm exec --x=y -- foo -z', t => {
+  process.argv = ['node', npx, '--x=y', '--no-install', 'foo', '-z']
   requireInject(npx, { [cli]: () => {} })
-  t.strictSame(process.argv, ['node', npm, 'exec', '--x=y', '--', 'foo', '-z'])
+  t.strictSame(process.argv, ['node', npm, 'exec', '--x=y', '--yes=false', '--', 'foo', '-z'])
+  t.end()
+})
+
+t.test('transform renamed options into proper values', t => {
+  process.argv = ['node', npx, '-y', '--shell=bash', '-p', 'foo', '-c', 'asdf']
+  requireInject(npx, { [cli]: () => {} })
+  t.strictSame(process.argv, ['node', npm, 'exec', '--yes', '--script-shell=bash', '--package', 'foo', '--call', 'asdf'])
+  t.end()
+})
+
+// warn if deprecated switches/options are used
+t.test('use a bunch of deprecated switches and options', t => {
+  process.argv = [
+    'node',
+    npx,
+    '--npm',
+    '/some/npm/bin',
+    '--node-arg=--harmony',
+    '-n',
+    '--require=foobar',
+    '--reg=http://localhost:12345/',
+    '-p',
+    'foo',
+    '--always-spawn',
+    '--shell-auto-fallback',
+    '--ignore-existing',
+    '-q',
+    'foobar'
+  ]
+
+  const expect = [
+    'node',
+    npm,
+    'exec',
+    '--registry',
+    'http://localhost:12345/',
+    '--package',
+    'foo',
+    '--loglevel',
+    'warn',
+    '--',
+    'foobar'
+  ]
+  requireInject(npx, { [cli]: () => {} })
+  t.strictSame(process.argv, expect)
+  t.strictSame(logs, [
+    [ 'npx: the --npm argument has been removed.' ],
+    [ 'npx: the --node-arg argument has been removed.' ],
+    [ 'npx: the --n argument has been removed.' ],
+    [ 'npx: the --always-spawn argument has been removed.' ],
+    [ 'npx: the --shell-auto-fallback argument has been removed.' ],
+    [ 'npx: the --ignore-existing argument has been removed.' ],
+    [ 'See `npm help exec` for more information' ]
+  ])
   t.end()
 })