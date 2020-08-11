b4d807c4c2c64eee1406ffca092dc6abbaa6153d
claudiahdz <cghr1990@gmail.com>
2020-08-11 16:59:51 -0500

    test(view): mock pacote instead of using npm-registry-mock

--- test/lib/view.js
+++ test/lib/view.js
@@ -1,11 +1,7 @@
 const t = require('tap')
-const mr = require('npm-registry-mock')
 const requireInject = require('require-inject')
 
-const REG = 'http://localhost:1331'
-
 let logs
-let server
 const cleanLogs = (done) => {
   logs = ''
   const fn = (...args) => {
@@ -16,238 +12,258 @@ const cleanLogs = (done) => {
   done()
 }
 
-t.beforeEach(cleanLogs)
-t.test('setup', (t) => {
-  var mocks = {
-    'get': {
-      '/red': [200, {
-        'name' : 'red',
-        'dist-tags': {
-          '1.0.1': {}
+const packument = (nv) => {
+  const mocks = {
+    'red': {
+      'name' : 'red',
+      'dist-tags': {
+        '1.0.1': {}
+      },
+      'time': {
+        'unpublished': new Date()
+      }
+    },
+    'blue': {
+      'name': 'blue',
+      'dist-tags': {},
+      'time': {
+        '1.0.0': '2019-08-06T16:21:09.842Z'
+      },
+      'versions': {
+        '1.0.0': {
+          'name': 'blue',
+          'version': '1.0.0',
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.blue.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1              
+          }
         },
-        'time': {
-          'unpublished': new Date()
+        '1.0.1': {}
+      }
+    },
+    'cyan': {
+      '_npmUser': {
+        'name': 'claudia',
+        'email': 'claudia@cyan.com'
+      } ,
+      'name': 'cyan',
+      'dist-tags': {},
+      'versions': {
+        '1.0.0': {
+          'version': '1.0.0',
+          'name': 'cyan',
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.cyan.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1
+          }
+        },
+        '1.0.1': {}
+      }
+    },
+    'brown': {
+      'name': 'brown'
+    },
+    'yellow': {
+      '_id': 'yellow',
+      'name': 'yellow',
+      'author': {
+        'name': 'foo',
+        'email': 'foo@yellow.com',
+        'twitter': 'foo'
+      },
+      'readme': 'a very useful readme',
+      'versions': {
+        '1.0.0': {
+          'version': '1.0.0',
+          'author': 'claudia',
+          'readme': 'a very useful readme',
+          'maintainers': [
+            { 'name': 'claudia', 'email': 'c@yellow.com', 'twitter': 'cyellow' },
+            { 'name': 'isaacs', 'email': 'i@yellow.com', 'twitter': 'iyellow' }
+          ]
+        },
+        '1.0.1': {
+          'version': '1.0.1',
+          'author': 'claudia'
+        },
+        '1.0.2': {
+          'version': '1.0.2',
+          'author': 'claudia'
         }
-      }],
-      '/blue': [200, {
-        'name': 'blue',
-        'dist-tags': {},
-        'time': {
-          '1.0.0': '2019-08-06T16:21:09.842Z'
+      }
+    },
+    'purple': {
+      'name': 'purple',
+      'versions': {
+        '1.0.0': {
+          'foo': 1,
+          'maintainers': [
+            { 'name': 'claudia' }
+          ]
         },
-        'versions': {
-          '1.0.0': {
-            'name': 'blue',
-            'version': 'blue',
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.blue.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1              
+        '1.0.1': {}
+      }
+    },
+    'green': {
+      '_id': 'green',
+      'name': 'green',
+      'dist-tags': {
+        'latest': '1.0.0'
+      },
+      'maintainers': [
+        { 'name': 'claudia', 'email': 'c@yellow.com', 'twitter': 'cyellow' },
+        { 'name': 'isaacs', 'email': 'i@yellow.com', 'twitter': 'iyellow' }
+      ],
+      'keywords': ['colors', 'green', 'crayola'],
+      'versions': {
+        '1.0.0': {
+          '_id': 'green',
+          'version': '1.0.0',
+          'description': 'green is a very important color',
+          'bugs': {
+            'url': 'http://bugs.green.com'
+          },
+          'deprecated': true,
+          'repository': {
+            'url': 'http://repository.green.com'
+          },
+          'license': { type: 'ACME' },
+          'bin': {
+            'green': 'bin/green.js'
+          },
+          'dependencies': {
+            'red': '1.0.0',
+            'yellow': '1.0.0'
+          },
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.green.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1
+          }
+        },
+        '1.0.1': {}
+      }
+    },
+    'black': {
+      'name': 'black',
+      'dist-tags': {
+        'latest': '1.0.0'
+      },
+      'versions': {
+        '1.0.0': {
+          'version': '1.0.0',
+          'bugs': 'http://bugs.black.com',
+          'license': {},
+          'dependencies': (() => {
+            const deps = {}
+            for (i = 0; i < 25; i++) {
+              deps[i] = '1.0.0'
             }
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/cyan': [200, {
-        '_npmUser': {
-          'name': 'claudia',
-          'email': 'claudia@cyan.com'
-        } ,
-        'name': 'cyan',
-        'dist-tags': {},
-        'versions': {
-          '1.0.0': {
-            'version': '1.0.0',
-            'name': 'cyan',
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.cyan.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1
-            }
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/brown': [200, {
-        'name': 'brown'
-      }],
-      '/yellow': [200, {
-        'name': 'yellow',
-        'author': {
-          'name': 'foo',
-          'email': 'foo@yellow.com',
-          'twitter': 'foo'
+            return deps
+          })(),
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.black.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1
+          }
         },
-        'readme': 'a very useful readme',
-        'versions': {
-          '1.0.0': {
-            'readme': 'a very useful readme',
-            'maintainers': [
-              { 'name': 'claudia', 'email': 'c@yellow.com', 'twitter': 'cyellow' },
-              { 'name': 'isaacs', 'email': 'i@yellow.com', 'twitter': 'iyellow' }
-            ]
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/purple': [200, {
-        'name': 'purple',
-        'versions': {
-          '1.0.0': {
-            'foo': 1,
-            'maintainers': [
-              { 'name': 'claudia' }
-            ]
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/green': [200, {
-        'name': 'green',
-        'dist-tags': {
-          'latest': '1.0.0'
+        '1.0.1': {}
+      }
+    },
+    'pink': {
+      'name': 'pink',
+      'dist-tags': {
+        'latest': '1.0.0'
+      },
+      'versions': {
+        '1.0.0': {
+          'version': '1.0.0',
+          'maintainers': [
+            { 'name': 'claudia', 'url': 'http://c.pink.com' },
+            { 'name': 'isaacs', 'url': 'http://i.pink.com'  }
+          ],
+          'repository': 'http://repository.pink.com',
+          'license': {},
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.pink.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1
+          }
         },
-        'keywords': ['colors', 'green', 'crayola'],
-        'versions': {
-          '1.0.0': {
-            'version': '1.0.0',
-            'bugs': {
-              'url': 'http://bugs.green.com'
-            },
-            'deprecated': true,
-            'repository': {
-              'url': 'http://repository.green.com'
-            },
-            'license': { type: 'ACME' },
-            'bin': {
-              'green': 'bin/green.js'
-            },
-            'dependencies': {
-              'red': '1.0.0',
-              'yellow': '1.0.0'
-            },
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.green.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1
-            }
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/black': [200, {
-        'name': 'black',
-        'dist-tags': {
-          'latest': '1.0.0'
+        '1.0.1': {}
+      }
+    },
+    'orange': {
+      'name': 'orange',
+      'dist-tags': {
+        'latest': '1.0.0'
+      },
+      'versions': {
+        '1.0.0': {
+          'version': '1.0.0',
+          'homepage': 'http://hm.orange.com',
+          'license': {},
+          'dist': {
+            'shasum': '123',
+            'tarball': 'http://hm.orange.com/1.0.0.tgz',
+            'integrity': '---',
+            'fileCount': 1,
+            'unpackedSize': 1
+          }
         },
-        'versions': {
-          '1.0.0': {
-            'version': '1.0.0',
-            'bugs': 'http://bugs.black.com',
-            'license': {},
-            'dependencies': (() => {
-              const deps = {}
-              for (i = 0; i < 25; i++) {
-                deps[i] = '1.0.0'
-              }
-              return deps
-            })(),
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.black.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1
-            }
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/pink': [200, {
-        'name': 'pink',
-        'dist-tags': {
-          'latest': '1.0.0'
-        },
-        'versions': {
-          '1.0.0': {
-            'version': '1.0.0',
-            'maintainers': [
-              { 'name': 'claudia', 'url': 'http://c.pink.com' },
-              { 'name': 'isaacs', 'url': 'http://i.pink.com'  }
-            ],
-            'repository': 'http://repository.pink.com',
-            'license': {},
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.pink.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1
-            }
-          },
-          '1.0.1': {}
-        }
-      }],
-      '/orange': [200, {
-        'name': 'orange',
-        'dist-tags': {
-          'latest': '1.0.0'
-        },
-        'versions': {
-          '1.0.0': {
-            'version': '1.0.0',
-            'homepage': 'http://hm.orange.com',
-            'license': {},
-            'dist': {
-              'shasum': '123',
-              'tarball': 'http://hm.orange.com/1.0.0.tgz',
-              'integrity': '---',
-              'fileCount': 1,
-              'unpackedSize': 1
-            }
-          },
-          '1.0.1': {}
-        }
-      }]
+        '1.0.1': {}
+      }
     }
   }
-  mr({ port: 1331, mocks }, (err, s) => {
-    server = s
-  })
-  t.done()
-})
+  return mocks[nv.name]
+}
 
+t.beforeEach(cleanLogs)
 t.test('should log package info', t => {
   const view = requireInject('../../lib/view.js', {
     '../../lib/npm.js': {
       flatOptions: {
-        registry: REG,
         global: false,
-        unicode: true
       }
+    },
+    'pacote': {
+      packument
     }
   })
 
   const viewJson = requireInject('../../lib/view.js', {
     '../../lib/npm.js': {
       flatOptions: {
-        registry: REG,
         json: true
       }
+    },
+    'pacote': {
+      packument
     }
   })
 
-  t.test('mkdirp@0.3.5', t => {
-    view(['mkdirp@0.3.5'], () => {
-      t.matchSnapshot(logs)
-      t.end()
-    })
+  const viewUnicode = requireInject('../../lib/view.js', {
+    '../../lib/npm.js': {
+      flatOptions: {
+        global: false,
+        unicode: true
+      }
+    },
+    'pacote': {
+      packument
+    }
   })
 
   t.test('package with license, bugs, repository and other fields', t => {
@@ -293,14 +309,14 @@ t.test('should log package info', t => {
   })
 
   t.test('package with no modified time', t => {
-    view(['cyan@1.0.0'], () => {
+    viewUnicode(['cyan@1.0.0'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
   })
 
   t.test('package with --json and semver range', t => {
-    viewJson(['underscore@^1.3.1'], () => {
+    viewJson(['cyan@^1.0.0'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
@@ -319,8 +335,8 @@ t.test('should log package info', t => {
 t.test('should log info of package in current working dir', t => {
   const testDir = t.testdir({
     'package.json': JSON.stringify({
-      name: 'underscore',
-      version: '1.3.1'
+      name: 'blue',
+      version: '1.0.0'
     }, null, 2)
   })
 
@@ -328,15 +344,17 @@ t.test('should log info of package in current working dir', t => {
     '../../lib/npm.js': {
       prefix: testDir,
       flatOptions: {
-        defaultTag: '1.3.1',
-        registry: REG,
+        defaultTag: '1.0.0',
         global: false
       }
+    },
+    'pacote': {
+      packument
     }
   })
 
   t.test('specific version', t => {
-    view(['.@1.3.1'], () => {
+    view(['.@1.0.0'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
@@ -356,19 +374,23 @@ t.test('should log info by field name', t => {
   const viewJson = requireInject('../../lib/view.js', {
     '../../lib/npm.js': {
       flatOptions: {
-        registry: REG,
         json: true,
         global: false
       }
+    },
+    'pacote': {
+      packument
     }
   })
 
   const view = requireInject('../../lib/view.js', {
     '../../lib/npm.js': {
       flatOptions: {
-        registry: REG,
         global: false
       }
+    },
+    'pacote': {
+      packument
     }
   })
 
@@ -380,21 +402,21 @@ t.test('should log info by field name', t => {
   })
 
   t.test('several fields', t => {
-    viewJson(['underscore@1.3.1', 'name', 'version', 'foo[bar]'], () => {
+    viewJson(['yellow@1.0.0', 'name', 'version', 'foo[bar]'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
   })
 
   t.test('several fields with several versions', t => {
-    view(['underscore@1.x.x', 'author'], () => {
+    view(['yellow@1.x.x', 'author'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
   })
 
   t.test('nested field with brackets', t => {
-    viewJson(['underscore@1.3.0', 'author[name]'], () => {
+    viewJson(['orange@1.0.0', 'dist[shasum]'], () => {
       t.matchSnapshot(logs)
       t.end()
     })
@@ -415,7 +437,7 @@ t.test('should log info by field name', t => {
   })
 
   t.test('unknown nested field ', t => {
-    view(['underscore@1.3.1', 'dist.foobar'], () => {
+    view(['yellow@1.0.0', 'dist.foobar'], () => {
       t.equals(logs, '\n', 'no info to display')
       t.end()
     })
@@ -476,9 +498,11 @@ t.test('throws when unpublished', (t) => {
     '../../lib/npm.js': {
       flatOptions: {
         defaultTag: '1.0.1',
-        registry: REG,
         global: false
       }
+    },
+    'pacote': {
+      packument
     }
   })
   view(['red'], (err) => {
@@ -492,13 +516,15 @@ t.test('completion', (t) => {
     '../../lib/npm.js': {
       flatOptions: {
         defaultTag: '1.0.1',
-        registry: REG,
         global: false
       }
+    },
+    'pacote': {
+      packument
     }
   })
   view.completion({
-    conf: { argv: { remain: ['npm', 'view', 'mkdirp@0.3.5'] } }
+    conf: { argv: { remain: ['npm', 'view', 'green@1.0.0'] } }
   }, (err, res) => {
     t.ok(res, 'returns back fields')
     t.end()
@@ -521,7 +547,3 @@ t.test('no registry completion', (t) => {
   })
 })
 
-t.test('cleanup', function (t) {
-  server.close()
-  t.done()
-})
