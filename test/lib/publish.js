a1f9be8a7f9b7a3a813fc3e5e705bc982470b0e2
isaacs <i@izs.me>
2020-10-27 15:25:20 -0700

    Support publishing any kind of spec, not just directories
    Credit: @isaacs
    Close: #2074
    Reviewed-by: @nlf

--- test/lib/publish.js
+++ test/lib/publish.js
@@ -7,6 +7,8 @@ const config = { list: [defaults] }
 const fs = require('fs')
 
 t.test('should publish with libnpmpublish, respecting publishConfig', (t) => {
+  t.plan(5)
+
   const publishConfig = { registry: 'https://some.registry' }
   const testDir = t.testdir({
     'package.json': JSON.stringify({
@@ -41,15 +43,14 @@ t.test('should publish with libnpmpublish, respecting publishConfig', (t) => {
         name: 'my-cool-pkg',
         version: '1.0.0',
       }))
-      return ''
+      return Buffer.from('')
     },
     libnpmpublish: {
-      publish: (arg, manifest, opts) => {
-        t.ok(arg, 'gets path')
-        t.ok(manifest, 'gets manifest')
+      publish: (manifest, tarData, opts) => {
+        t.match(manifest, { name: 'my-cool-pkg', version: '1.0.0' }, 'gets manifest')
+        t.isa(tarData, Buffer, 'tarData is a buffer')
         t.ok(opts, 'gets opts object')
         t.same(opts.registry, publishConfig.registry, 'publishConfig is passed through')
-        t.ok(true, 'libnpmpublish is called')
       },
     },
   })
@@ -57,11 +58,12 @@ t.test('should publish with libnpmpublish, respecting publishConfig', (t) => {
   publish([testDir], (er) => {
     if (er)
       throw er
-    t.end()
+    t.pass('got to callback')
   })
 })
 
 t.test('re-loads publishConfig if added during script process', (t) => {
+  t.plan(5)
   const publishConfig = { registry: 'https://some.registry' }
   const testDir = t.testdir({
     'package.json': JSON.stringify({
@@ -94,15 +96,14 @@ t.test('re-loads publishConfig if added during script process', (t) => {
         version: '1.0.0',
         publishConfig,
       }))
-      return ''
+      return Buffer.from('')
     },
     libnpmpublish: {
-      publish: (arg, manifest, opts) => {
-        t.ok(arg, 'gets path')
-        t.ok(manifest, 'gets manifest')
+      publish: (manifest, tarData, opts) => {
+        t.match(manifest, { name: 'my-cool-pkg', version: '1.0.0' }, 'gets manifest')
+        t.isa(tarData, Buffer, 'tarData is a buffer')
         t.ok(opts, 'gets opts object')
         t.same(opts.registry, publishConfig.registry, 'publishConfig is passed through')
-        t.ok(true, 'libnpmpublish is called')
       },
     },
   })
@@ -110,11 +111,13 @@ t.test('re-loads publishConfig if added during script process', (t) => {
   publish([testDir], (er) => {
     if (er)
       throw er
-    t.end()
+    t.pass('got to callback')
   })
 })
 
 t.test('should not log if silent', (t) => {
+  t.plan(2)
+
   const testDir = t.testdir({
     'package.json': JSON.stringify({
       name: 'my-cool-pkg',
@@ -133,29 +136,38 @@ t.test('should not log if silent', (t) => {
     },
     '../../lib/utils/tar.js': {
       getContents: () => ({}),
-      logTar: () => {},
+      logTar: () => {
+        t.pass('called logTar (but nothing actually printed)')
+      },
     },
     '../../lib/utils/otplease.js': (opts, fn) => {
       return Promise.resolve().then(() => fn(opts))
     },
+    '../../lib/utils/output.js': () => {
+      throw new Error('should not output in silent mode')
+    },
     npmlog: {
       verbose: () => {},
+      notice: () => {},
       level: 'silent',
     },
     libnpmpack: async () => '',
     libnpmpublish: {
-      publish: () => {},
+      publish: (manifest, tarData, opts) => {
+        throw new Error('should not call libnpmpublish!')
+      },
     },
   })
 
   publish([testDir], (er) => {
     if (er)
       throw er
-    t.end()
+    t.pass('got to callback')
   })
 })
 
 t.test('should log tarball contents', (t) => {
+  t.plan(3)
   const testDir = t.testdir({
     'package.json': JSON.stringify({
       name: 'my-cool-pkg',
@@ -177,29 +189,32 @@ t.test('should log tarball contents', (t) => {
         id: 'someid',
       }),
       logTar: () => {
-        t.ok(true, 'logTar is called')
+        t.pass('logTar is called')
       },
     },
     '../../lib/utils/output.js': () => {
-      t.ok(true, 'output fn is called')
+      t.pass('output fn is called')
     },
     '../../lib/utils/otplease.js': (opts, fn) => {
       return Promise.resolve().then(() => fn(opts))
     },
     libnpmpack: async () => '',
     libnpmpublish: {
-      publish: () => {},
+      publish: () => {
+        throw new Error('should not call libnpmpublish!')
+      },
     },
   })
 
   publish([testDir], (er) => {
     if (er)
       throw er
-    t.end()
+    t.pass('got to callback')
   })
 })
 
 t.test('shows usage with wrong set of arguments', (t) => {
+  t.plan(1)
   const publish = requireInject('../../lib/publish.js', {
     '../../lib/npm.js': {
       flatOptions: {
@@ -210,13 +225,11 @@ t.test('shows usage with wrong set of arguments', (t) => {
     },
   })
 
-  publish(['a', 'b', 'c'], (er) => {
-    t.matchSnapshot(er, 'should print usage')
-    t.end()
-  })
+  publish(['a', 'b', 'c'], (er) => t.matchSnapshot(er, 'should print usage'))
 })
 
 t.test('throws when invalid tag', (t) => {
+  t.plan(1)
   const publish = requireInject('../../lib/publish.js', {
     '../../lib/npm.js': {
       flatOptions: {
@@ -228,7 +241,66 @@ t.test('throws when invalid tag', (t) => {
   })
 
   publish([], (err) => {
-    t.ok(err, 'throws when tag name is a valid SemVer range')
-    t.end()
+    t.match(err, {
+      message: /Tag name must not be a valid SemVer range: /,
+    }, 'throws when tag name is a valid SemVer range')
+  })
+})
+
+t.test('can publish a tarball', t => {
+  t.plan(3)
+  const testDir = t.testdir({
+    package: {
+      'package.json': JSON.stringify({
+        name: 'my-cool-tarball',
+        version: '1.2.3',
+      }),
+    },
+  })
+  const tar = require('tar')
+  tar.c({
+    cwd: testDir,
+    file: `${testDir}/package.tgz`,
+    sync: true,
+  }, ['package'])
+
+  // no cheating!  read it from the tarball.
+  fs.unlinkSync(`${testDir}/package/package.json`)
+  fs.rmdirSync(`${testDir}/package`)
+
+  const tarFile = fs.readFileSync(`${testDir}/package.tgz`)
+  const publish = requireInject('../../lib/publish.js', {
+    '../../lib/npm.js': {
+      flatOptions: {
+        json: true,
+        defaultTag: 'latest',
+      },
+      config,
+    },
+    '../../lib/utils/tar.js': {
+      getContents: () => ({
+        id: 'someid',
+      }),
+      logTar: () => {},
+    },
+    '../../lib/utils/output.js': () => {},
+    '../../lib/utils/otplease.js': (opts, fn) => {
+      return Promise.resolve().then(() => fn(opts))
+    },
+    libnpmpublish: {
+      publish: (manifest, tarData, opts) => {
+        t.match(manifest, {
+          name: 'my-cool-tarball',
+          version: '1.2.3',
+        }, 'sent manifest to lib pub')
+        t.strictSame(tarData, tarFile, 'sent the tarball data to lib pub')
+      },
+    },
+  })
+
+  publish([`${testDir}/package.tgz`], (er) => {
+    if (er)
+      throw er
+    t.pass('got to callback')
   })
 })
