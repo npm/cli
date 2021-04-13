42965fdfa08072e2b172d777569900522de63802
Gar <gar+gh@danger.computer>
2021-04-13 10:20:22 -0700

    libnpmversion@1.1.1

--- node_modules/@npmcli/git/lib/opts.js
+++ node_modules/@npmcli/git/lib/opts.js
@@ -1,6 +1,11 @@
-const gitEnv = require('./env.js')
+// Values we want to set if they're not already defined by the end user
+// This defaults to accepting new ssh host key fingerprints
+const gitEnv = {
+  GIT_ASKPASS: 'echo',
+  GIT_SSH_COMMAND: 'ssh -oStrictHostKeyChecking=accept-new'
+}
 module.exports = (opts = {}) => ({
   stdioString: true,
   ...opts,
-  env: opts.env || gitEnv(),
+  env: opts.env || { ...gitEnv, ...process.env }
 })
