231b9633ee43765221dec3e61ba78f852d7ed264
Gar <gar+gh@danger.computer>
2021-04-22 15:00:24 -0700

    npm prune

--- node_modules/libtap/lib/diags.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const objToYaml = require('./obj-to-yaml.js')
-
-module.exports = extra => (y => y ? '\n' + y : '')(objToYaml(extra))
--- node_modules/libtap/versions.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-  libtap: require('./package.json').version,
-  tapParser: require('tap-parser/package.json').version,
-  tapYaml: require('tap-yaml/package.json').version,
-  tcompare: require('tcompare/package.json').version
-}
