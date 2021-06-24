ac656ea702efa21c143c464eb8bdccbc5768dece
isaacs <i@izs.me>
2021-06-24 12:21:02 -0700

    wip: make coverage mapping a bit better

--- test/coverage-map.js
+++ test/coverage-map.js
@@ -1,11 +1,17 @@
 const full = process.env.npm_lifecycle_event === 'check-coverage'
 const coverageMap = (filename) => {
-  if (full && /load-all.js$/.test(filename)) {
+  const { basename } = require('path')
+  const testbase = basename(filename)
+  if (full && testbase === 'load-all.js') {
     const glob = require('glob')
     const { resolve, relative } = require('path')
     const dir = resolve(__dirname, '../lib')
     return glob.sync(`${dir}/**/*.js`)
       .map(f => relative(process.cwd(), f))
+  }
+  if (testbase === 'load-all-commands.js') {
+    const { cmdList } = require('../lib/utils/cmd-list.js')
+    return cmdList.map(cmd => `lib/${cmd}.js`).concat('lib/utils/usage.js')
   }
   if (/windows-shims\.js$/.test(filename)) {
     // this one doesn't provide any coverage nyc can track
