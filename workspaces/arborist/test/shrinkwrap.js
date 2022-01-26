8c3b143ca20d0da56c0ce2764e288a4c203b9f93
linkgoron <Linkgoron@users.noreply.github.com>
2022-01-26 22:59:44 +0200

    fix(arborist): shrinkwrap throws when trying to read a folder without permissions (#4258)
    * fix(arborist): shrinkwrap throws trying to read a folder without permissions
    
    Fix an issue where shrinkwrap throws an error when trying to read
    a folder that it doesn't have permissions to, instead of returning
    a correct object with an error

--- workspaces/arborist/test/shrinkwrap.js
+++ workspaces/arborist/test/shrinkwrap.js
@@ -1597,4 +1597,19 @@ t.test('setting lockfileVersion from the file contents', async t => {
   })
 
   t.equal(Shrinkwrap.defaultLockfileVersion, 2, 'default is 2')
+
+  t.test('load should return error correctly when it cant access folder',
+    { skip: process.platform === 'win32' ? 'skip chmod in windows' : false },
+    async t => {
+      const dir = t.testdir({})
+      try {
+        fs.chmodSync(dir, '000')
+        const res = await Shrinkwrap.load({ path: dir })
+        t.ok(res.loadingError, 'loading error should exist')
+        t.strictSame(res.loadingError.errno, -13)
+        t.strictSame(res.loadingError.code, 'EACCES')
+      } finally {
+        fs.chmodSync(dir, '666')
+      }
+    })
 })
