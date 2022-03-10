4a647cad5d307148f55583bb982fb10298ab2b20
Gar <gar+gh@danger.computer>
2022-03-10 10:43:47 -0800

    fix: omit bots from authors

--- scripts/update-authors.sh
+++ scripts/update-authors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git log --use-mailmap --reverse --format='%aN <%aE>' | perl -wnE '
+git log --use-mailmap --reverse --format='%aN <%aE>' | grep -v "\[bot\]" | perl -wnE '
 BEGIN {
   say "# Authors sorted by whether or not they\x27re me";
 }
