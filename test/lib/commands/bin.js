0a957f5e2fbcce51c407d22b19e38004d09c51af
Gar <gar+gh@danger.computer>
2022-03-24 09:09:26 -0700

    fix: consolidate path delimiter logic

--- test/lib/commands/bin.js
+++ test/lib/commands/bin.js
@@ -1,60 +1,32 @@
 const t = require('tap')
-const { relative, join } = require('path')
 const { load: loadMockNpm } = require('../../fixtures/mock-npm')
 const mockGlobals = require('../../fixtures/mock-globals')
 
-const mockBin = async (t, { args = [], config = {} } = {}) => {
-  const { npm, outputs, ...rest } = await loadMockNpm(t, {
-    config,
-  })
-  const cmd = await npm.cmd('bin')
-  await npm.exec('bin', args)
-
-  return {
-    npm,
-    cmd,
-    bin: outputs[0][0],
-    ...rest,
-  }
-}
-
-t.test('bin', async t => {
-  const { cmd, bin, prefix, outputErrors } = await mockBin(t, {
-    config: { global: false },
-  })
-
-  t.match(cmd.usage, 'bin', 'usage has command name in it')
-  t.equal(relative(prefix, bin), join('node_modules/.bin'), 'prints the correct directory')
-  t.strictSame(outputErrors, [])
+t.test('bin not global', async t => {
+  const { npm, joinedOutput, logs } = await loadMockNpm(t)
+  await npm.exec('bin', [])
+  t.match(joinedOutput(), npm.localBin)
+  t.match(logs.error, [])
 })
 
-t.test('bin -g', async t => {
-  mockGlobals(t, { 'process.platform': 'posix' })
-  const { globalPrefix, bin, outputErrors } = await mockBin(t, {
+t.test('bin global in env.path', async t => {
+  const { npm, joinedOutput, logs } = await loadMockNpm(t, {
     config: { global: true },
   })
 
-  t.equal(relative(globalPrefix, bin), 'bin', 'prints the correct directory')
-  t.strictSame(outputErrors, [])
+  mockGlobals(t, {
+    'process.env': { path: npm.globalBin },
+  })
+  await npm.exec('bin', [])
+  t.match(joinedOutput(), npm.globalBin)
+  t.match(logs.error, [])
 })
 
-t.test('bin -g win32', async t => {
-  mockGlobals(t, { 'process.platform': 'win32' })
-  const { globalPrefix, bin, outputErrors } = await mockBin(t, {
+t.test('bin not in path', async t => {
+  const { npm, joinedOutput, logs } = await loadMockNpm(t, {
     config: { global: true },
   })
-
-  t.equal(relative(globalPrefix, bin), '', 'prints the correct directory')
-  t.strictSame(outputErrors, [])
-})
-
-t.test('bin -g (not in path)', async t => {
-  const { logs } = await mockBin(t, {
-    config: { global: true },
-    globals: {
-      'process.env.PATH': 'emptypath',
-    },
-  })
-
-  t.strictSame(logs.error[0], ['bin', '(not in PATH env variable)'])
+  await npm.exec('bin', [])
+  t.match(joinedOutput(), npm.globalBin)
+  t.match(logs.error, [['bin', '(not in PATH env variable)']])
 })
