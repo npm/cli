a80787093a5a636a0acd7598599954e8e2f38150
nlf <quitlahok@gmail.com>
2022-03-24 11:18:21 -0700

    fix(arborist): handle link nodes in old lockfiles correctly

--- workspaces/arborist/test/arborist/build-ideal-tree.js
+++ workspaces/arborist/test/arborist/build-ideal-tree.js
@@ -1163,6 +1163,26 @@ This is a one-time fix-up, please be patient...
   ])
 })
 
+t.test('inflating a link node in an old lockfile skips registry', async t => {
+  const checkLogs = warningTracker()
+  const path = resolve(fixtures, 'old-lock-with-link')
+  const arb = new Arborist({ path, ...OPT, registry: 'http://invalid.host' })
+  const tree = await arb.buildIdealTree()
+  t.matchSnapshot(printTree(tree))
+  t.strictSame(checkLogs(), [
+    [
+      'warn',
+      'old lockfile',
+      `
+The package-lock.json file was created with an old version of npm,
+so supplemental metadata must be fetched from the registry.
+
+This is a one-time fix-up, please be patient...
+`,
+    ],
+  ])
+})
+
 t.test('warn for ancient lockfile, even if we use v1', async t => {
   const checkLogs = warningTracker()
   const path = resolve(fixtures, 'sax')
