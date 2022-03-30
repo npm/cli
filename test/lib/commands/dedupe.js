2a26e5e21af788f025a5731d88f15f6dc88b4c0c
Gar <gar+gh@danger.computer>
2022-03-30 15:37:01 -0700

    fix: remove dedupe --save
    * Removed dedupe --save documentation and attempted implementation.
    * Remove some unneeded otplease mocks from test
    
    `npm dedupe --save` didn't work in a easy to understand way. It would
    only update a top level dependency that was duplicated in the tree.
    Found this out rewriting the dedupe tests to be real.  This is not very
    intuitive and it's best if folks use update or install for saving to
    package.json.

--- test/lib/commands/dedupe.js
+++ test/lib/commands/dedupe.js
@@ -1,5 +1,9 @@
 const t = require('tap')
 const { load: loadMockNpm } = require('../../fixtures/mock-npm')
+const path = require('path')
+const fs = require('fs')
+
+const MockRegistry = require('../../fixtures/mock-registry.js')
 
 t.test('should throw in global mode', async (t) => {
   const { npm } = await loadMockNpm(t, {
@@ -14,45 +18,78 @@ t.test('should throw in global mode', async (t) => {
   )
 })
 
-t.test('should remove dupes using Arborist', async (t) => {
-  t.plan(5)
-  const { npm } = await loadMockNpm(t, {
-    mocks: {
-      '@npmcli/arborist': function (args) {
-        t.ok(args, 'gets options object')
-        t.ok(args.path, 'gets path option')
-        t.ok(args.dryRun, 'gets dryRun from user')
-        this.dedupe = () => {
-          t.ok(true, 'dedupe is called')
-        }
-      },
-      '../../lib/utils/reify-finish.js': (npm, arb) => {
-        t.ok(arb, 'gets arborist tree')
-      },
-    },
-    config: {
-      'dry-run': 'true',
-    },
-  })
-  await npm.exec('dedupe', [])
-})
+const testTop = {
+  name: 'test-top',
+  version: '1.0.0',
+  dependencies: {
+    'test-dep-a': '*',
+    'test-dep-b': '*',
+  },
+}
+const testDepA = {
+  name: 'test-dep-a',
+  version: '1.0.1',
+  dependencies: { 'test-sub': '*' },
+}
+const testDepB = {
+  name: 'test-dep-b',
+  version: '1.0.0',
+  dependencies: { 'test-sub': '*' },
+}
+const testSub = {
+  name: 'test-sub',
+  version: '1.0.0',
+}
 
-t.test('should remove dupes using Arborist - no arguments', async (t) => {
-  t.plan(2)
-  const { npm } = await loadMockNpm(t, {
-    mocks: {
-      '@npmcli/arborist': function (args) {
-        t.ok(args.dryRun, 'gets dryRun from config')
-        t.ok(args.save, 'gets user-set save value from config')
-        this.dedupe = () => {}
+const treeWithDupes = {
+  'package.json': JSON.stringify(testTop),
+  node_modules: {
+    'test-dep-a': {
+      'package.json': JSON.stringify(testDepA),
+      node_modules: {
+        'test-sub': {
+          'package.json': JSON.stringify(testSub),
+        },
       },
-      '../../lib/utils/reify-output.js': () => {},
-      '../../lib/utils/reify-finish.js': () => {},
     },
-    config: {
-      'dry-run': true,
-      save: true,
+    'test-dep-b': {
+      'package.json': JSON.stringify(testDepB),
+      node_modules: {
+        'test-sub': {
+          'package.json': JSON.stringify(testSub),
+        },
+      },
+    },
+  },
+}
+
+t.test('dedupe', async (t) => {
+  const { npm, joinedOutput } = await loadMockNpm(t, {
+    prefixDir: treeWithDupes,
+  })
+  const registry = new MockRegistry({
+    tap: t,
+    registry: npm.config.get('registry'),
+  })
+  const manifestSub = registry.manifest({
+    name: 'test-sub',
+    packuments: [{ version: '1.0.0' }],
+  })
+
+  await registry.package({
+    manifest: manifestSub,
+    tarballs: {
+      '1.0.0': path.join(npm.prefix, 'node_modules', 'test-dep-a', 'node_modules', 'test-sub'),
     },
   })
   await npm.exec('dedupe', [])
+  t.match(joinedOutput(), /added 1 package, and removed 2 packages/)
+  t.ok(fs.existsSync(path.join(npm.prefix, 'node_modules', 'test-sub')), 'test-sub was hoisted')
+  t.notOk(
+    fs.existsSync(path.join(npm.prefix, 'node_modules', 'test-dep-a', 'node_modules', 'test-sub')),
+    'test-dep-a/test-sub was removed'
+  )
+  t.notOk(
+    fs.existsSync(path.join(npm.prefix, 'node_modules', 'test-dep-b', 'node_modules', 'test-sub')),
+    'test-dep-b/test-sub was removed')
 })
