4d63d893f3a3f7906440e7454d2b3afd9a67fa60
Ruy Adorno <ruyadorno@hotmail.com>
2022-05-02 13:20:35 -0400

    chore: remove bot from authors
    Replaces: https://github.com/npm/cli/pull/4814
    
    Co-authored-by: Gar <gar+gh@danger.computer>

--- scripts/update-authors.sh
+++ scripts/update-authors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git log --use-mailmap --reverse --format='%aN <%aE>' | grep -v "\[bot\]" | perl -wnE '
+git log --use-mailmap --reverse --format='%aN <%aE>' | grep -v "\[bot\]" | grep -v "^npm team" | perl -wnE '
 BEGIN {
   say "# Authors sorted by whether or not they\x27re me";
 }
