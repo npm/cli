b102668bc9ca8bd101e0b61e25f7db7cc4317ed6
nlf <quitlahok@gmail.com>
2022-05-10 10:11:17 -0700

    chore: add npm CLI robot to mailmap and exclude from AUTHORS

--- scripts/update-authors.sh
+++ scripts/update-authors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git log --use-mailmap --reverse --format='%aN <%aE>' | grep -v "\[bot\]" | grep -v "^npm team" | perl -wnE '
+git log --use-mailmap --reverse --format='%aN <%aE>' | grep -v -e "\[bot\]" -e "^npm team" -e "^npm CLI robot" | perl -wnE '
 BEGIN {
   say "# Authors sorted by whether or not they\x27re me";
 }
