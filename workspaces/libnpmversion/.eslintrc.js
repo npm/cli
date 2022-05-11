6e41711c1597c5a1e41a61df86be9007319d61af
Gar <gar+gh@danger.computer>
2022-05-11 08:46:07 -0700

    chore: @npmcli/template-oss@3.5.0

--- workspaces/libnpmversion/.eslintrc.js
+++ workspaces/libnpmversion/.eslintrc.js
@@ -1,5 +1,7 @@
 /* This file is automatically added by @npmcli/template-oss. Do not edit. */
 
+'use strict'
+
 const { readdirSync: readdir } = require('fs')
 
 const localConfigs = readdir(__dirname)
