13831bf882dd74da99e43c27b78e5ba6610b035c
Julian Møller Ellehauge <git@jumoel.com>
2022-05-25 10:20:23 +0200

    Update snapshot

--- tap-snapshots/test/lib/utils/config/describe-all.js.test.cjs
+++ tap-snapshots/test/lib/utils/config/describe-all.js.test.cjs
@@ -1765,8 +1765,8 @@ When set to \`dev\` or \`development\`, this is an alias for \`--include=dev\`.
 
 * Default: "legacy"
 * Type: "legacy", "webauthn", "sso", "saml", or "oauth"
-* DEPRECATED: This method of SSO/SAML/OAuth is deprecated and will be removed
-  in a future version of npm in favor of web-based login.
+* DEPRECATED: The SSO/SAML/OAuth methods are deprecated and will be removed in
+  a future version of npm in favor of web-based login.
 
 What authentication strategy to use with \`adduser\`/\`login\`.
 
