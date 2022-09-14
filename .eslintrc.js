b365d4709498a8f0b047737d0e9eb491c9c6b02c
Luke Karrys <luke@lukekarrys.com>
2022-09-14 13:32:41 -0700

    chore: @npmcli/template-oss@4.2.0
    This also updates the root of this repo to be managed by template-oss.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+/* This file is automatically added by @npmcli/template-oss. Do not edit. */
+
+'use strict'
+
 const { readdirSync: readdir } = require('fs')
 
 const localConfigs = readdir(__dirname)
@@ -6,6 +10,22 @@ const localConfigs = readdir(__dirname)
 
 module.exports = {
   root: true,
+  ignorePatterns: [
+    'docs',
+    'smoke-tests',
+    'workspaces/arborist',
+    'workspaces/libnpmaccess',
+    'workspaces/libnpmdiff',
+    'workspaces/libnpmexec',
+    'workspaces/libnpmfund',
+    'workspaces/libnpmhook',
+    'workspaces/libnpmorg',
+    'workspaces/libnpmpack',
+    'workspaces/libnpmpublish',
+    'workspaces/libnpmsearch',
+    'workspaces/libnpmteam',
+    'workspaces/libnpmversion',
+  ],
   extends: [
     '@npmcli',
     ...localConfigs,
---
+++ scripts/template-oss/index.js
@@ -0,0 +1,56 @@
+module.exports = {
+  rootRepo: {
+    add: {
+      '.github/ISSUE_TEMPLATE/config.yml': false,
+      '.github/ISSUE_TEMPLATE/bug.yml': false,
+      '.github/dependabot.yml': false,
+      '.github/workflows/post-dependabot.yml': false,
+      '.github/workflows/ci.yml': 'ci.yml',
+      '.github/workflows/release.yml': 'release.yml',
+    },
+  },
+  workspaceRepo: {
+    add: {
+      '.github/workflows/release.yml': false,
+      '.github/workflows/release-please.yml': false,
+    },
+  },
+  lockfile: true,
+  npm: '.',
+  defaultBranch: 'latest',
+  ciVersions: [
+    '12.13.0',
+    '12.x',
+    '14.15.0',
+    '14.x',
+    '16.0.0',
+    '16.x',
+  ],
+  releaseBranches: [
+    'v8',
+  ],
+  distPaths: [
+    'index.js',
+    'docs/content/**/*.md',
+    'docs/output/**/*.html',
+    'man',
+  ],
+  allowPaths: [
+    '/node_modules/',
+    '/index.js',
+    '/Makefile',
+    '/make.bat',
+    '/DEPENDENCIES.md',
+    '/CONTRIBUTING.md',
+    '/configure',
+    '/changelogs/',
+    '/AUTHORS',
+    '/.mailmap',
+    '/.licensee.json',
+    '/.gitattributes',
+  ],
+  ignorePaths: [
+    '/node_modules/.bin/',
+    '/node_modules/.cache/',
+  ],
+}
