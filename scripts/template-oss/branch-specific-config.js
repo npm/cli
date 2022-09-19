7ee629d4189ad6e2e645e24f3e62eb2d99e9d56a
Luke Karrys <luke@lukekarrys.com>
2022-09-19 14:11:16 -0700

    chore: template-oss@4.3.2

---
+++ scripts/template-oss/branch-specific-config.js
@@ -0,0 +1,5 @@
+// Leave this empty to use the default ciVersions from template-oss
+// This file is kept here to make it easier to apply template-oss
+// changes to other branches which might have different ciVersions
+// or other conifg options
+module.exports = {}
