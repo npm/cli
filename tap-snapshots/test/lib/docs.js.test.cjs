a09e19d88f046e54e8d75343883635a1bd056310
nlf <quitlahok@gmail.com>
2022-10-06 12:50:36 -0700

    feat: introduce the `npm config fix` command

--- tap-snapshots/test/lib/docs.js.test.cjs
+++ tap-snapshots/test/lib/docs.js.test.cjs
@@ -2650,6 +2650,7 @@ npm config get [<key> [<key> ...]]
 npm config delete <key> [<key> ...]
 npm config list [--json]
 npm config edit
+npm config fix
 
 Options:
 [--json] [-g|--global] [--editor <editor>] [-L|--location <global|user|project>]
@@ -2665,6 +2666,7 @@ npm config get [<key> [<key> ...]]
 npm config delete <key> [<key> ...]
 npm config list [--json]
 npm config edit
+npm config fix
 
 alias: c
 \`\`\`
