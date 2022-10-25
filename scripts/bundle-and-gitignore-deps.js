df68f31cc3dfcff53ba43bc207cf9bfa9ba34b53
Luke Karrys <luke@lukekarrys.com>
2022-10-25 16:47:58 -0700

    chore: update scripts

--- scripts/bundle-and-gitignore-deps.js
+++ scripts/bundle-and-gitignore-deps.js
@@ -1,41 +1,22 @@
 const Arborist = require('@npmcli/arborist')
-const { resolve } = require('path')
-const ignore = resolve(__dirname, '../node_modules/.gitignore')
-const { writeFileSync } = require('fs')
-const pj = resolve(__dirname, '../package.json')
-const pkg = require(pj)
-const bundle = []
-const arb = new Arborist({ path: resolve(__dirname, '..') })
-const shouldIgnore = []
+const packlist = require('npm-packlist')
+const { join, relative } = require('path')
+const localeCompare = require('@isaacs/string-locale-compare')('en')
+const PackageJson = require('@npmcli/package-json')
+const { run, CWD, git, fs } = require('./util')
 
-// disabling to get linting to pass, this file is going away soon
-// eslint-disable-next-line
-arb.loadVirtual().then(tree => {
-  // eslint-disable-next-line
-  for (const node of tree.children.values()) {
-    const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
-    const nonProdWorkspace =
-      node.isWorkspace && !(has(tree.package.dependencies, node.name))
-    if (node.dev || nonProdWorkspace) {
-      console.error('ignore', node.name)
-      shouldIgnore.push(node.name)
-    } else if (tree.edgesOut.has(node.name)) {
-      console.error('BUNDLE', node.name)
-      bundle.push(node.name)
-    }
-  }
-  pkg.bundleDependencies = bundle.sort((a, b) => a.localeCompare(b, 'en'))
-
-  const ignores = shouldIgnore.sort((a, b) => a.localeCompare(b, 'en'))
-    .map(i => `/${i}`)
-    .join('\n')
-  const ignoreData = `# Automatically generated to ignore dev deps
-/.package-lock.json
+const ALWAYS_IGNORE = `
+.bin/
+.cache/
 package-lock.json
 CHANGELOG*
 changelog*
+ChangeLog*
+Changelog*
 README*
 readme*
+ReadMe*
+Readme*
 __pycache__
 .editorconfig
 .idea/
@@ -55,9 +36,204 @@ __pycache__
 .babelrc*
 .nyc_output
 .gitkeep
-
-${ignores}
 `
-  writeFileSync(ignore, ignoreData)
-  writeFileSync(pj, JSON.stringify(pkg, 0, 2) + '\n')
-})
+
+const lsIgnored = async (dir, { removeIgnoredFiles }) => {
+  const files = await git(
+    'ls-files',
+    '--cached',
+    '--ignored',
+    `--exclude-standard`,
+    dir,
+    { lines: true }
+  )
+
+  if (removeIgnoredFiles) {
+    for (const file of files) {
+      await git('rm', file)
+    }
+    return []
+  }
+
+  return files
+}
+
+const getAllowedPaths = (files) => {
+  // Get all files within node_modules and remove
+  // the node_modules/ portion of the path for processing
+  // since this list will go inside a gitignore at the
+  // root of the node_modules dir
+  const nmFiles = files
+    .filter(f => f.startsWith('node_modules/'))
+    .map(f => f.replace(/^node_modules\//, ''))
+    .sort(localeCompare)
+
+  class AllowSegments {
+    #segments
+    #usedSegments
+
+    constructor (pathSegments, rootSegments = []) {
+      // Copy strings with spread operator since we mutate these arrays
+      this.#segments = [...pathSegments]
+      this.#usedSegments = [...rootSegments]
+    }
+
+    get next () {
+      return this.#segments[0]
+    }
+
+    get remaining () {
+      return this.#segments
+    }
+
+    get used () {
+      return this.#usedSegments
+    }
+
+    use () {
+      const segment = this.#segments.shift()
+      this.#usedSegments.push(segment)
+      return segment
+    }
+
+    allowContents ({ use = true, isDirectory = true } = {}) {
+      if (use) {
+        this.use()
+      }
+      // Allow a previously ignored directy
+      // Important: this should NOT have a trailing
+      // slash if we are not sure it is a directory.
+      // Since a dep can be a directory or a symlink and
+      // a trailing slash in a .gitignore file
+      // tells git to treat it only as a directory
+      return [`!/${this.used.join('/')}${isDirectory ? '/' : ''}`]
+    }
+
+    allow ({ use = true } = {}) {
+      if (use) {
+        this.use()
+      }
+      // Allow a previously ignored directory but ignore everything inside
+      return [
+        ...this.allowContents({ use: false, isDirectory: true }),
+        `/${this.used.join('/')}/*`,
+      ]
+    }
+  }
+
+  const gatherAllows = (pathParts, usedParts) => {
+    const ignores = []
+    const segments = new AllowSegments(pathParts, usedParts)
+
+    if (segments.next) {
+      // 1) Process scope segment of the path, if it has one
+      if (segments.next.startsWith('@')) {
+        // For scoped deps we need to allow the entire scope dir
+        // due to how gitignore works. Without this the gitignore will
+        // never look to allow our bundled dep since the scope dir was ignored.
+        // It ends up looking like this for `@colors/colors`:
+        //
+        // # Allow @colors dir
+        // !/@colors/
+        // # Ignore everything inside. This is safe because there is
+        // # nothing inside a scope except other packages
+        // !/colors/*
+        //
+        // Then later we will allow the specific dep inside that scope.
+        // This way if a scope includes bundled AND unbundled deps,
+        // we only allow the bundled ones.
+        ignores.push(...segments.allow())
+      }
+
+      // 2) Now we process the name segment of the path
+      // and allow the dir and everything inside of it (like source code, etc)
+      ignores.push(...segments.allowContents({ isDirectory: false }))
+
+      // 3) If we still have remaining segments and the next segment
+      // is a nested node_modules directory...
+      if (segments.next && segments.use() === 'node_modules') {
+        ignores.push(
+          // Allow node_modules and ignore everything inside of it
+          // Set false here since we already "used" the node_modules path segment
+          ...segments.allow({ use: false }),
+          // Repeat the process with the remaining path segments to include whatever is left
+          ...gatherAllows(segments.remaining, segments.used)
+        )
+      }
+    }
+
+    return ignores
+  }
+
+  const allowPaths = new Set()
+  for (const file of nmFiles) {
+    for (const allow of gatherAllows(file.split('/'))) {
+      allowPaths.add(allow)
+    }
+  }
+
+  return [...allowPaths]
+}
+
+const setBundleDeps = async () => {
+  const pkg = await PackageJson.load(CWD)
+
+  pkg.update({
+    bundleDependencies: Object.keys(pkg.content.dependencies).sort(localeCompare),
+  })
+
+  await pkg.save()
+
+  return pkg.content.bundleDependencies
+}
+
+/*
+This file sets what is checked in to node_modules. The root .gitignore file
+includes node_modules and this file writes an ignore file to
+node_modules/.gitignore. We ignore everything and then use a query to find all
+the bundled deps and allow each one of those explicitly.
+
+Since node_modules can be nested we have to process each portion of the path and
+allow it while also ignoring everything inside of it, with the exception of a
+deps source. We have to do this since everything is ignored by default, and git
+will not allow a nested path if its parent has not also been allowed. BUT! We
+also have to ignore other things in those directories.
+*/
+const main = async ({ removeIgnoredFiles }) => {
+  await setBundleDeps()
+
+  const arb = new Arborist({ path: CWD })
+  const files = await arb.loadActual().then(packlist)
+
+  const ignoreFile = [
+    '# Automatically generated to ignore everything except bundled deps',
+    '# Ignore everything by default except this file',
+    '/*',
+    '!/.gitignore',
+    '# Allow all bundled deps',
+    ...getAllowedPaths(files),
+    '# Always ignore some specific patterns within any allowed package',
+    ...ALWAYS_IGNORE.trim().split('\n'),
+  ]
+
+  const NODE_MODULES = join(CWD, 'node_modules')
+  const res = await fs.writeFile(join(NODE_MODULES, '.gitignore'), ignoreFile.join('\n'))
+
+  // After we write the file we have to check if any of the paths already checked in
+  // inside node_modules are now going to be ignored. If we find any then fail with
+  // a list of paths that will need to have `git rm` run on them.
+  const trackedAndIgnored = await lsIgnored(NODE_MODULES, { removeIgnoredFiles })
+
+  if (trackedAndIgnored.length) {
+    const message = [
+      'The following files are checked in to git but will now be ignored.',
+      `Rerun this script with \`--remove-ignored-files\` to remove them.`,
+      ...trackedAndIgnored.map(p => relative(NODE_MODULES, p)),
+    ].join('\n')
+    throw new Error(message)
+  }
+
+  return res
+}
+
+run(main)
--- scripts/dependency-graph.js
+++ scripts/dependency-graph.js
@@ -1,14 +1,14 @@
-'use strict'
+const Arborist = require('@npmcli/arborist')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const log = require('proc-log')
+const { run, CWD, pkg, fs } = require('./util.js')
 
 // Generates our dependency graph documents in DEPENDENCIES.md.
 
-const Arborist = require('@npmcli/arborist')
-const fs = require('fs')
-
 // To re-create npm-cli-repos.txt run:
-/* eslint-disable-next-line max-len */
-// npx --package=@npmcli/stafftools@latest gh repos --json | json -a name | sort > scripts/npm-cli-repos.txt
-const repos = fs.readFileSync('./scripts/npm-cli-repos.txt', 'utf8').trim().split('\n')
+// npx -p @npmcli/stafftools gh repos --json | json -a name | sort > scripts/npm-cli-repos.txt
+const repos = readFileSync(join(CWD, 'scripts', 'npm-cli-repos.txt'), 'utf-8').trim().split('\n')
 
 // these have a different package name than the repo name, and are ours.
 const aliases = {
@@ -66,6 +66,7 @@ function escapeName (name) {
   }
   return name
 }
+
 function stripName (name) {
   if (name.startsWith('@')) {
     const parts = name.slice(1).split('/')
@@ -75,21 +76,19 @@ function stripName (name) {
 }
 
 const main = async function () {
-  const arborist = new Arborist({
-    prefix: process.cwd(),
-    path: process.cwd(),
-  })
-  const tree = await arborist.loadVirtual({ path: process.cwd(), name: 'npm' })
+  // add all of the cli's public workspaces as package names
+  for (const { name, pkg: ws } of await pkg.mapWorkspaces()) {
+    if (!ws.private) {
+      repos.push(name)
+    }
+  }
+
+  const arborist = new Arborist({ prefix: CWD, path: CWD })
+  const tree = await arborist.loadVirtual({ path: CWD, name: 'npm' })
   tree.name = 'npm'
 
-  const {
-    heirarchy: heirarchyOurs,
-    annotations: annotationsOurs,
-  } = walk(tree, true)
-
-  const {
-    annotations: annotationsAll,
-  } = walk(tree, false)
+  const [annotationsOurs, heirarchyOurs] = walk(tree, true)
+  const [annotationsAll] = walk(tree, false)
 
   const out = [
     '# npm dependencies',
@@ -114,39 +113,61 @@ const main = async function () {
     '',
     ` - ${heirarchyOurs.reverse().join('\n - ')}`,
   ]
-  fs.writeFileSync('DEPENDENCIES.md', out.join('\n'))
-  console.log('wrote to DEPENDENCIES.md')
+
+  return fs.writeFile(join(CWD, 'DEPENDENCIES.md'), out.join('\n'))
 }
 
 const walk = function (tree, onlyOurs) {
   const annotations = [] // mermaid dependency annotations
   const dependedBy = {}
+
   iterate(tree, dependedBy, annotations, onlyOurs)
+
   const allDeps = new Set(Object.keys(dependedBy))
   const foundDeps = new Set()
   const heirarchy = []
-  while (allDeps.size) {
-    const level = []
-    for (const dep of allDeps) {
-      if (!dependedBy[dep].size) {
-        level.push(dep)
-        foundDeps.add(dep)
+
+  if (onlyOurs) {
+    while (allDeps.size) {
+      log.silly('SIZE', allDeps.size)
+      const level = []
+
+      for (const dep of allDeps) {
+        log.silly(dep, '::', [...dependedBy[dep]].join(', '))
+        log.silly('-'.repeat(80))
+
+        if (!dependedBy[dep].size) {
+          level.push(dep)
+          foundDeps.add(dep)
+        }
       }
-    }
-    for (const dep of allDeps) {
-      for (const found of foundDeps) {
-        allDeps.delete(found)
-        dependedBy[dep].delete(found)
+
+      log.silly('LEVEL', level.length)
+      log.silly('FOUND', foundDeps.size)
+
+      for (const dep of allDeps) {
+        for (const found of foundDeps) {
+          allDeps.delete(found)
+          dependedBy[dep].delete(found)
+        }
       }
+
+      log.silly('SIZE', allDeps.size)
+
+      if (!level.length) {
+        const remaining = `Remaining deps: ${[...allDeps.keys()]}`
+        throw new Error(`Would do an infinite loop here, need to debug. ${remaining}`)
+      }
+
+      heirarchy.push(level.join(', '))
+      log.silly('HIEARARCHY', heirarchy.length)
+      log.silly('='.repeat(80))
     }
-    if (!level.length) {
-      throw new Error('Would do an infinite loop here, need to debug')
-    }
-    heirarchy.push(level.join(', '))
   }
 
-  return { heirarchy, annotations }
+  return [annotations, heirarchy]
 }
+
 const iterate = function (node, dependedBy, annotations, onlyOurs) {
   if (!dependedBy[node.packageName]) {
     dependedBy[node.packageName] = new Set()
@@ -166,11 +187,4 @@ const iterate = function (node, dependedBy, annotations, onlyOurs) {
   }
 }
 
-main().then(() => {
-  process.exit(0)
-  return 0
-}).catch(err => {
-  console.error(err)
-  process.exit(1)
-  return 1
-})
+run(main)
