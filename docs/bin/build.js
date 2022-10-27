36f39a1798ca0b472e6464967bacd8529f4f49e7
Luke Karrys <luke@lukekarrys.com>
2022-10-27 11:06:47 -0700

    chore: remove mdx from docs build
    We will be updating to mdx@2 soon which is esm only, but built in to
    Gatsby. Due to the new documentation build process here and in
    `npm/documentation`, there is much less need for us to verify all our
    docs here. Therefore, it is best to remove this dependency now to avoid
    needing to update it here at the same time as the docs site.

--- docs/bin/build.js
+++ docs/bin/build.js
@@ -1,10 +1,7 @@
 const run = require('../lib/build.js')
 const { paths } = require('../lib/index')
 
-run({
-  verify: true,
-  ...paths,
-})
+run(paths)
   .then((res) => console.log(`Wrote ${res.length} files`))
   .catch((err) => {
     process.exitCode = 1
