1d3c53b92a744514e5482cba5700cedbcc949be1
Luke Karrys <luke@lukekarrys.com>
2022-11-01 14:30:40 -0700

    chore: ignore-scripts when installing docs deps

--- scripts/publish.js
+++ scripts/publish.js
@@ -85,7 +85,7 @@ const main = async (opts) => {
   }
 
   await npm('prune', '--omit=dev', '--no-save', '--no-audit', '--no-fund')
-  await npm('install', '-w', 'docs')
+  await npm('install', '-w', 'docs', '--ignore-scripts', '--no-audit', '--no-fund')
   await git.dirty()
 
   for (const p of publishes) {
