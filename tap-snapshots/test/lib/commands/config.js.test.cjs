1f4c7eaa362d99d5ca3eb97601810c9644712259
Brian DeHamer <bdehamer@github.com>
2023-02-09 12:11:27 -0800

    feat: add provenance attestation
    This adds a new `--provenance` flag to npm for provenance attestation
    during `npm publish`
    
    If set to `true`, npm will detect if it is running in GitHub actions and
    will generate an appropriate attestation bundle for that environment.
    
    The primary work in this PR was done by
    [@bdehamer](https://github.com/bdehamer), with some cleanup and
    edge-case handling added by the npm cli team.

--- tap-snapshots/test/lib/commands/config.js.test.cjs
+++ tap-snapshots/test/lib/commands/config.js.test.cjs
@@ -116,6 +116,7 @@ exports[`test/lib/commands/config.js TAP config list --json > output matches sna
   "preid": "",
   "production": null,
   "progress": true,
+  "provenance": false,
   "proxy": null,
   "read-only": false,
   "rebuild-bundle": true,
@@ -270,6 +271,7 @@ prefer-online = false
 preid = "" 
 production = null 
 progress = true 
+provenance = false 
 proxy = null 
 read-only = false 
 rebuild-bundle = true 
