79bfd03947a25f4bfb67d1c54893be7c79ec77e2
Philip Harrison <philip@mailharrison.com>
2023-02-14 13:39:17 +0000

    feat: audit signatures verifies attestations
    Update `audit signatures` to also verify Sigstore attestations.
    
    Additional changes:
    - Adding error message to json error output as there are a lot of different failure cases with signature verification that would be hard to debug without this
    - Adding predicateType to json error output for attestations to diffentiate between provenance and publish attestations
    
    References:
    - Pacote changes: https://github.com/npm/pacote/pull/259
    - RFC: https://github.com/npm/rfcs/pull/626
    
    Signed-off-by: Philip Harrison <philip@mailharrison.com>

--- tap-snapshots/test/lib/commands/audit.js.test.cjs
+++ tap-snapshots/test/lib/commands/audit.js.test.cjs
@@ -53,6 +53,7 @@ exports[`test/lib/commands/audit.js TAP audit signatures json output with invali
   "invalid": [
     {
       "code": "EINTEGRITYSIGNATURE",
+      "message": "kms-demo@1.0.0 has an invalid registry signature with keyid: SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA and signature: bogus",
       "integrity": "sha512-QqZ7VJ/8xPkS9s2IWB7Shj3qTJdcRyeXKbPQnsZjsPEwvutGv0EGeVchPcauoiDFJlGbZMFq5GDCurAGNSghJQ==",
       "keyid": "SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA",
       "location": "node_modules/kms-demo",
@@ -76,11 +77,34 @@ exports[`test/lib/commands/audit.js TAP audit signatures json output with invali
 }
 `
 
+exports[`test/lib/commands/audit.js TAP audit signatures json output with invalid attestations > must match snapshot 1`] = `
+{
+  "invalid": [
+    {
+      "code": "EATTESTATIONVERIFY",
+      "message": "sigstore@1.0.0 failed to verify attestation: artifact signature verification failed",
+      "integrity": "sha512-e+qfbn/zf1+rCza/BhIA//Awmf0v1pa5HQS8Xk8iXrn9bgytytVLqYD0P7NSqZ6IELTgq+tcDvLPkQjNHyWLNg==",
+      "keyid": "",
+      "location": "node_modules/sigstore",
+      "name": "sigstore",
+      "registry": "https://registry.npmjs.org/",
+      "resolved": "https://registry.npmjs.org/sigstore/-/sigstore-1.0.0.tgz",
+      "signature": "MEYCIQD10kAn3lC/1rJvXBtSDckbqkKEmz369gPDKb4lG4zMKQIhAP1+RhbMcASsfXhxpXKNCAjJb+3Av3Br95eKD7VL/BEB",
+      "predicateType": "https://slsa.dev/provenance/v0.2",
+      "type": "dependencies",
+      "version": "1.0.0"
+    }
+  ],
+  "missing": []
+}
+`
+
 exports[`test/lib/commands/audit.js TAP audit signatures json output with invalid signatures > must match snapshot 1`] = `
 {
   "invalid": [
     {
       "code": "EINTEGRITYSIGNATURE",
+      "message": "kms-demo@1.0.0 has an invalid registry signature with keyid: SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA and signature: bogus",
       "integrity": "sha512-QqZ7VJ/8xPkS9s2IWB7Shj3qTJdcRyeXKbPQnsZjsPEwvutGv0EGeVchPcauoiDFJlGbZMFq5GDCurAGNSghJQ==",
       "keyid": "SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA",
       "location": "node_modules/kms-demo",
@@ -173,6 +197,17 @@ audited 1 package in xxx
 
 `
 
+exports[`test/lib/commands/audit.js TAP audit signatures with invalid attestations > must match snapshot 1`] = `
+audited 1 package in xxx
+
+1 package has an invalid attestation:
+
+sigstore@1.0.0 (https://registry.npmjs.org/)
+
+Someone might have tampered with this package since it was published on the registry!
+
+`
+
 exports[`test/lib/commands/audit.js TAP audit signatures with invalid signatures > must match snapshot 1`] = `
 audited 1 package in xxx
 
@@ -201,6 +236,18 @@ audited 1 package in xxx
 1 package has a missing registry signature but the registry is providing signing keys:
 
 kms-demo@1.0.0 (https://registry.npmjs.org/)
+`
+
+exports[`test/lib/commands/audit.js TAP audit signatures with multiple invalid attestations > must match snapshot 1`] = `
+audited 2 packages in xxx
+
+2 packages have invalid attestations:
+
+sigstore@1.0.0 (https://registry.npmjs.org/)
+tuf-js@1.0.0 (https://registry.npmjs.org/)
+
+Someone might have tampered with these packages since they were published on the registry!
+
 `
 
 exports[`test/lib/commands/audit.js TAP audit signatures with multiple invalid signatures > must match snapshot 1`] = `
@@ -245,6 +292,15 @@ audited 2 packages in xxx
 1 package has a missing registry signature but the registry is providing signing keys:
 
 async@1.1.1 (https://registry.npmjs.org/)
+`
+
+exports[`test/lib/commands/audit.js TAP audit signatures with valid attestations > must match snapshot 1`] = `
+audited 1 package in xxx
+
+1 package has a verified registry signature
+
+1 package has a verified attestation
+
 `
 
 exports[`test/lib/commands/audit.js TAP audit signatures with valid signatures > must match snapshot 1`] = `
