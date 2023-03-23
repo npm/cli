631bf89d6f767c624a050591d072716011dc7410
Gar <gar+gh@danger.computer>
2023-03-23 13:52:32 -0700

    fix: do less work looking up commands

--- smoke-tests/tap-snapshots/test/index.js.test.cjs
+++ smoke-tests/tap-snapshots/test/index.js.test.cjs
@@ -24,12 +24,13 @@ All commands:
     access, adduser, audit, bugs, cache, ci, completion,
     config, dedupe, deprecate, diff, dist-tag, docs, doctor,
     edit, exec, explain, explore, find-dupes, fund, get, help,
-    hook, init, install, install-ci-test, install-test, link,
-    ll, login, logout, ls, org, outdated, owner, pack, ping,
-    pkg, prefix, profile, prune, publish, query, rebuild, repo,
-    restart, root, run-script, search, set, shrinkwrap, star,
-    stars, start, stop, team, test, token, uninstall, unpublish,
-    unstar, update, version, view, whoami
+    help-search, hook, init, install, install-ci-test,
+    install-test, link, ll, login, logout, ls, org, outdated,
+    owner, pack, ping, pkg, prefix, profile, prune, publish,
+    query, rebuild, repo, restart, root, run-script, search,
+    set, shrinkwrap, star, stars, start, stop, team, test,
+    token, uninstall, unpublish, unstar, update, version, view,
+    whoami
 
 Specify configs in the ini-formatted file:
     {NPM}/{TESTDIR}/project/.npmrc
