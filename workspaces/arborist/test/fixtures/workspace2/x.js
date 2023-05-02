fc11df57a34ed35d61cc7e4a2b43d89045ef3a50
micheci <michecimartinez@gmail.com>
2023-05-02 16:57:23 -0500

    added spanish

--- workspaces/arborist/test/fixtures/workspace2/x.js
+++
@@ -1,4 +0,0 @@
-const loadActual = require('../../../lib/load-actual.js')
-loadActual(__dirname).then(tree => {
-  console.log(require('util').inspect(tree, { depth: Infinity }))
-})
--- workspaces/arborist/test/fixtures/workspaces-link-bin/packages/b/create-file.js
+++
@@ -1,1 +0,0 @@
-require('fs').writeFileSync('file.js', '')
