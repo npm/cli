6069ccececa214c6dbe9e3098d240e71fca64cb0
Mike Ribbons <mribbons@gmail.com>
2023-06-09 21:22:44 +1000

    add ps1 scripts
    Resolves UNC path issues because powershell.exe
    supports UNC, while cmd.exe doesn't.
    (Which is why npm.cmd doesn't work within
    UNC paths, even under powershell)
    
    Resolves https://github.com/npm/cli/issues/6280

---
+++ bin/npx.ps1
@@ -0,0 +1,26 @@
+#!/usr/bin/env pwsh
+$basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+$exe=""
+if ($PSVersionTable.PSVersion -lt "6.0" -or $IsWindows) {
+  # Fix case when both the Windows and Linux builds of Node
+  # are installed in the same directory
+  $exe=".exe"
+}
+$ret=0
+
+$nodebin = $(Get-Command "node$exe" -ErrorAction SilentlyContinue -ErrorVariable F).Source
+if ($nodebin -eq $null) {
+  Write-Host "node$exe not found."
+  exit 1
+}
+$nodedir = $(New-Object -ComObject Scripting.FileSystemObject).GetFile("$nodebin").ParentFolder.Path
+
+# Support pipeline input
+if ($MyInvocation.ExpectingInput) {
+  $input | & "node$exe" "$nodedir/node_modules/npm/bin/npx-cli.js" $args
+} else {
+  & "node$exe" "$nodedir/node_modules/npm/bin/npx-cli.js" $args
+}
+$ret=$LASTEXITCODE
+exit $ret
