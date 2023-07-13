834edca0b78129d101b8e16a032048517ac32bca
AaronHamilton965 <91709196+AaronHamilton965@users.noreply.github.com>
2023-07-13 21:26:29 +0000

    added --no-install tap test

--- test/lib/commands/exec.js
+++ test/lib/commands/exec.js
@@ -3,6 +3,8 @@ const fs = require('fs/promises')
 const path = require('path')
 const { load: loadMockNpm } = require('../../fixtures/mock-npm.js')
 const MockRegistry = require('@npmcli/mock-registry')
+const { packument } = require('pacote')
+const { version } = require('os')
 
 t.test('call with args', async t => {
   const { npm } = await loadMockNpm(t, {
@@ -128,4 +130,39 @@ t.test('workspaces', async t => {
   await npm.exec('exec', ['@npmcli/npx-test'])
   const exists = await fs.stat(path.join(npm.prefix, 'workspace-a', 'npm-exec-test-success'))
   t.ok(exists.isFile(), 'bin ran, creating file inside workspace')
+})
+
+t.test('npx --no-install @npmcli/npx-test', async t => {
+  const registry = new MockRegistry({
+    tap: t,
+    registry: 'https://registry.npmjs.org/',
+  })
+
+  const manifest = registry.manifest({ name: '@npmcli/npx-test'})
+  manifest.versions['1.0.0'].bin = { 'npx-test': 'index.js' }
+
+  const { npm } = await loadMockNpm(t, {
+    config: {
+      audit: false,
+      yes: false,
+    },
+    prefixDir: {
+      'npm-exec-test': {
+        'package.json': JSON.stringify(manifest),
+        'index.js': `#!/usr/bin/env node
+  require('fs').writeFileSync('npm-exec-test-success', '')`,
+      },
+    },
+  })
+
+  try {
+    await npm.exec('exec', ['@npmcli/npx-test'])
+    t.fail('Expected error was not thrown')
+  } catch (error) {
+    t.match(
+      error.message,
+      'npx canceled due to missing packages and no YES option: ', 
+      'Expected error message thrown'
+    )
+  }
 })
