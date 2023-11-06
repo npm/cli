0d1d79ff0eaa52a0ff7baf2fa9c764a4b761cd63
Jamie Tanna <jamie@jamietanna.co.uk>
2023-11-06 09:27:45 +0000

    Correctly handle license objects in SBOM generation
    As a means to resolve #6966, we can tweak the way we handle licenses,
    where receiving a license object, instead of license string, results in
    a malformed SPDX JSON SBOM.
    
    While working on this, it was noted that CycloneDX also needed to be
    amended, as it was omitting any license objects.
    
    Closes #6966.

--- test/lib/utils/sbom-spdx.js
+++ test/lib/utils/sbom-spdx.js
@@ -117,6 +117,20 @@ t.test('single node - with single license', t => {
   t.end()
 })
 
+t.test('single node - with license object', t => {
+  const pkg = {
+    ...rootPkg,
+    license: {
+      type: 'MIT',
+      url: 'http://github.com/kriskowal/q/raw/master/LICENSE',
+    },
+  }
+  const node = { ...root, package: pkg }
+  const res = spdxOutput({ npm, nodes: [node] })
+  t.matchSnapshot(JSON.stringify(res))
+  t.end()
+})
+
 t.test('single node - with license expression', t => {
   const pkg = { ...rootPkg, license: '(MIT OR Apache-2.0)' }
   const node = { ...root, package: pkg }
