0b61e23fcab9bf68c6bc8cf1d39661c624baf7f8
siemhesda <143130929+siemhesda@users.noreply.github.com>
2023-11-22 09:57:54 -0800

    Revert "implement logic using deepStrictEqual"
    This reverts commit 37a2a75a7536d45de8b122078ab58c177136abd2.

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-
-const { deepStrictEqual } = require("assert");
-
-//Object.entries
-const resultFromObjectEntriesFn = Object.entries(process.env).filter(item => item[0].includes("_package_config_"));
-
-//Object.fromEntries
-const resultFromEntriesFn = Object.fromEntries(resultFromObjectEntriesFn);
-
-//compare using deepStrictEqual - > comp
-deepStrictEqual(resultFromEntriesFn, {
-  npm_package_config_array_0: "item1",
-  npm_package_config_array_1: "item2",
-  npm_package_config_array_2: "item3"
-})
+if (require.main === module) {
+  require('./lib/cli.js')(process)
+} else {
+  throw new Error('The programmatic API was removed in npm v8.0.0')
+}
