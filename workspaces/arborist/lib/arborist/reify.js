f28837d552fb3906da8a1a450843ba978fa1298e
Gar <gar+gh@danger.computer>
2024-02-13 19:59:16 -0800

    remove single-use deduper mixin

--- workspaces/arborist/lib/arborist/reify.js
+++ workspaces/arborist/lib/arborist/reify.js
@@ -1592,4 +1592,22 @@ module.exports = cls => class Reifier extends cls {
       }
     }
   }
+
+  async dedupe (options = {}) {
+    // allow the user to set options on the ctor as well.
+    // XXX: deprecate separate method options objects.
+    options = { ...this.options, ...options }
+    const tree = await this.loadVirtual().catch(() => this.loadActual())
+    const names = []
+    for (const name of tree.inventory.query('name')) {
+      if (tree.inventory.query('name', name).size > 1) {
+        names.push(name)
+      }
+    }
+    return this.reify({
+      ...options,
+      preferDedupe: true,
+      update: { names },
+    })
+  }
 }
