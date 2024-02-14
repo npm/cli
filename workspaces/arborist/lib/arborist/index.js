f175fa565ed237114b5012ea356d755a1f6e7b36
Gar <gar+gh@danger.computer>
2024-02-13 20:39:14 -0800

    remove single-use audit mixin

--- workspaces/arborist/lib/arborist/index.js
+++ workspaces/arborist/lib/arborist/index.js
@@ -30,11 +30,11 @@ const { resolve } = require('path')
 const { homedir } = require('os')
 const { depth } = require('treeverse')
 const { saveTypeMap } = require('../add-rm-pkg-deps.js')
+const AuditReport = require('../audit-report.js')
 const mapWorkspaces = require('@npmcli/map-workspaces')
 
 const mixins = [
   require('../tracker.js'),
-  require('./audit.js'),
   require('./build-ideal-tree.js'),
   require('./load-actual.js'),
   require('./load-virtual.js'),
@@ -175,6 +175,47 @@ class Arborist extends Base {
     }
 
     return node
+  }
+
+  async audit (options = {}) {
+    this.addTracker('audit')
+    if (this.options.global) {
+      throw Object.assign(
+        new Error('`npm audit` does not support testing globals'),
+        { code: 'EAUDITGLOBAL' }
+      )
+    }
+
+    // allow the user to set options on the ctor as well.
+    // XXX: deprecate separate method options objects.
+    options = { ...this.options, ...options }
+
+    process.emit('time', 'audit')
+    let tree
+    if (options.packageLock === false) {
+      // build ideal tree
+      await this.loadActual(options)
+      await this.buildIdealTree()
+      tree = this.idealTree
+    } else {
+      tree = await this.loadVirtual()
+    }
+    if (this.options.workspaces.length) {
+      options.filterSet = this.workspaceDependencySet(
+        tree,
+        this.options.workspaces,
+        this.options.includeWorkspaceRoot
+      )
+    }
+    if (!options.workspacesEnabled) {
+      options.filterSet =
+        this.excludeWorkspacesDependencySet(tree)
+    }
+    this.auditReport = await AuditReport.load(tree, options)
+    const ret = options.fix ? this.reify(options) : this.auditReport
+    process.emit('timeEnd', 'audit')
+    this.finishTracker('audit')
+    return ret
   }
 }
 
