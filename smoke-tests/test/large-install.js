6df7ac99c550b0b7a8a6dbd5c43cf2a5f4a8a6de
Luke Karrys <luke@lukekarrys.com>
2024-03-30 16:18:00 -0700

    fix: ensure maxSockets is respected
    Fixes https://github.com/npm/cli/issues/7072
    
    deps: agent-base@7.1.1
    
    chore: add smoke-test for large prod installs

---
+++ smoke-tests/test/large-install.js
@@ -0,0 +1,33 @@
+const t = require('tap')
+const path = require('path')
+const setup = require('./fixtures/setup.js')
+
+const getFixture = (p) => require(
+  path.resolve(__dirname, './fixtures/large-install', p))
+
+const runTest = async (t) => {
+  const { npm } = await setup(t, {
+    // This test relies on the actual production registry
+    mockRegistry: false,
+    testdir: {
+      project: {
+        'package.json': getFixture('package.json'),
+        'package-lock.json': getFixture('package-lock.json'),
+      },
+    },
+  })
+  return npm('install', '--no-audit', '--no-fund')
+}
+
+// This test was originally created for https://github.com/npm/cli/issues/6763
+// but was later removed after the corresponding Node bug was fixed and the test
+// started flaking. These flakes were most likely a sign that there was actually
+// a bug where maxSockets were not being respected  https://github.com/npm/cli/issues/7072.
+// So now this test has been brought back and if it does start flaking again it
+// should be investigated.
+t.test('large install', async t => {
+  const { stdout } = await runTest(t)
+  // As a bonus this test asserts that this package-lock always
+  // installs the same number of packages.
+  t.match(stdout, /^added 1265 packages in/)
+})
