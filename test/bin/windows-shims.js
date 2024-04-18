451aaa5c154ad472f74f6fadbf0b0a717fbdbd4d
Luke Karrys <luke@lukekarrys.com>
2024-04-18 14:15:55 -0700

    comment

--- test/bin/windows-shims.js
+++ test/bin/windows-shims.js
@@ -100,6 +100,7 @@ t.test('run shims', t => {
     },
   })
 
+  // make sure to chmod node too
   for (const shim of ['node.exe', ...Object.keys(SHIMS)]) {
     chmodSync(join(path, shim), 0o755)
   }
