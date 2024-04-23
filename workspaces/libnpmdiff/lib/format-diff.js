1c063e815d95a272ef9df94725a1444f9595f843
Gar <gar+gh@danger.computer>
2024-04-23 13:19:21 -0700

    fix: inline color selector in libnpmdiff
    Eventually we'll use npm to do this so we don't have to check if color
    is enabled

--- workspaces/libnpmdiff/lib/format-diff.js
+++ workspaces/libnpmdiff/lib/format-diff.js
@@ -1,7 +1,23 @@
-const colorizeDiff = require('@npmcli/disparity-colors')
 const jsDiff = require('diff')
 
 const shouldPrintPatch = require('./should-print-patch.js')
+
+const colors = {
+  // red
+  removed: { open: '\x1B[31m', close: '\x1B[39m' },
+  // green
+  added: { open: '\x1B[32m', close: '\x1B[39m' },
+  // blue
+  header: { open: '\x1B[34m', close: '\x1B[39m' },
+  // cyan
+  section: { open: '\x1B[36m', close: '\x1B[39m' },
+}
+
+const color = (colorStr, colorId) => {
+  const { open, close } = colors[colorId]
+  // avoid highlighting the "\n" (would highlight till the end of the line)
+  return colorStr.replace(/[^\n\r]+/g, open + '$&' + close)
+}
 
 const formatDiff = ({ files, opts = {}, refs, versions }) => {
   let res = ''
@@ -83,9 +99,17 @@ const formatDiff = ({ files, opts = {}, refs, versions }) => {
       header(`+++ ${names.b}`)
     }
 
-    res += (opts.color
-      ? colorizeDiff(patch, { headerLength })
-      : patch)
+    if (opts.color) {
+      // this RegExp will include all the `\n` chars into the lines, easier to join
+      const lines = patch.split(/^/m)
+      res += color(lines.slice(0, headerLength).join(''), 'header')
+      res += lines.slice(headerLength).join('')
+        .replace(/^-.*/gm, color('$&', 'removed'))
+        .replace(/^\+.*/gm, color('$&', 'added'))
+        .replace(/^@@.+@@/gm, color('$&', 'section'))
+    } else {
+      res += patch
+    }
   }
 
   return res.trim()
