0ced0b766b0c323fa6a31f759a0d0025d6fa35da
Luke Karrys <luke@lukekarrys.com>
2024-04-29 12:16:27 -0700

    fix: move run-script banners to stderr when in json mode
    Fixes #7354

--- docs/bin/build.js
+++ docs/bin/build.js
@@ -16,7 +16,7 @@ const run = require('../lib/build.js')
 const { paths } = require('../lib/index')
 
 run(paths)
-  .then((res) => console.log(`Wrote ${res.length} files`))
+  .then((res) => console.error(`Wrote ${res.length} files`))
   .catch((err) => {
     process.exitCode = 1
     console.error(err)
