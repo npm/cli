2f6c6581ae12afd207e8abfa6f62219636cf7537
Luke Karrys <luke@lukekarrys.com>
2024-04-30 14:29:57 -0700

    chore(smoke-tests): force pack json test to use npmLocal

--- smoke-tests/test/pack-json-output.js
+++ smoke-tests/test/pack-json-output.js
@@ -5,7 +5,7 @@ const setup = require('./fixtures/setup.js')
 t.test('pack --json returns only json on stdout', async t => {
   const { npmLocal } = await setup(t)
 
-  const { stderr, stdout } = await npmLocal('pack', '--json')
+  const { stderr, stdout } = await npmLocal('pack', '--json', { force: true })
 
   t.match(stderr, /> npm@.* prepack\n/, 'stderr has banner')
   t.ok(JSON.parse(stdout), 'stdout can be parsed as json')
