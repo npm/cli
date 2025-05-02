6ac55415ae57b8eda59e7b355903c520f1926214
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-02 09:37:28 -0400

    remove test that doesn't work in command prompt

--- test/bin/windows-shims.js
+++ test/bin/windows-shims.js
@@ -276,7 +276,6 @@ t.test('run shims', t => {
     { bin: 'npm', params: ['test -- --param="hello world"'], expected: `--param=hello world` },
     { bin: 'npm', params: ['test -- a=1,b=2,c=3'], expected: `a=1,b=2,c=3` },
     { bin: 'npm', params: ['test -- "a=1,b=2,c=3"'], expected: `a=1,b=2,c=3` },
-    { bin: 'npm', params: ['test -- \'a=1,b=2,c=3\''], expected: `a=1,b=2,c=3` },
   ]
 
   // ensure that all tests are either run or skipped
