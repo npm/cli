360b09099020b0bb556b44edabb408077729e07e
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-05 11:34:16 -0400

    also for npx.ps1

--- bin/npx.ps1
+++ bin/npx.ps1
@@ -36,10 +36,16 @@ function Normalize {
     return """$Path"""
 }
 
-$NPX_PositionMessage = $MyInvocation.PositionMessage -split "`r?`n"
-$firstIndex = $NPX_PositionMessage[2].IndexOf("~")
-$lastIndex = $NPX_PositionMessage[2].LastIndexOf("~")
-$NPX_OG_COMMAND = $NPX_PositionMessage[1].Substring($firstIndex, $lastIndex - $firstIndex + 1)
+$firstPartOfString = $MyInvocation.Line.Substring($MyInvocation.OffsetInLine - 1, $MyInvocation.Line.length - $MyInvocation.OffsetInLine + 1)
+
+$splitStringArray = $MyInvocation.Line.Substring($MyInvocation.OffsetInLine - 1, $MyInvocation.Line.length - $MyInvocation.OffsetInLine + 1) -split "``;"
+for ($i = 0; $i -lt $splitStringArray.Length; $i++) {
+	$splitString = $splitStringArray[$i]
+        if ($splitString.IndexOf(";") -ne -1) {
+		$splitStringArray[$i] = $splitString.Substring(0, $splitString.IndexOf(";"))
+	}
+}
+$NPX_OG_COMMAND = $splitStringArray[0..$i] -join "``;"
 
 $NPX_ARGS = $NPX_OG_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
 $INVOKE_NPX = "& $(Normalize $NODE_EXE) $(Normalize $NPX_CLI_JS) $NPX_ARGS"
