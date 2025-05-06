d74a744642a74cde7551e5830ed14b658748e6ea
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-06 13:29:05 -0400

    combine tests

--- test/bin/windows-shims.js
+++ test/bin/windows-shims.js
@@ -276,15 +276,9 @@ t.test('run shims', t => {
     { bin: 'npm', params: ['help'], expected: `npm@${version} ${ROOT}` },
     { bin: 'npx', params: ['--version'], expected: version },
     { bin: 'npm', params: ['test'], expected: '' },
-    { bin: 'npm', params: ['test -- hello world'], expected: `hello\nworld` },
-    { bin: 'npm', params: ['test -- -p hello'], expected: `-p\nhello` },
-    { bin: 'npm', params: ['test -- -p "hello world"'], expected: `-p\nhello world` },
-    { bin: 'npm', params: ['test -- --param=hello'], expected: `--param=hello` },
-    { bin: 'npm', params: ['test -- --param="hello world"'], expected: `--param=hello world` },
+    { bin: 'npm', params: ['test -- hello world -p hello -g "hello world" --param1=hello world --param2="hello world"'], expected: `hello\nworld\n-p\nhello\n-g\nhello world\n--param1=hello\nworld\n--param1=hello world` },
     { bin: 'npm', params: ['test -- a=1,b=2,c=3'], expected: `a=1,b=2,c=3` },
-    { bin: 'npm', params: ['test -- "a=1,b=2,c=3"'], expected: `a=1,b=2,c=3` },
     { bin: 'npx', params: ['. -- a=1,b=2,c=3'], expected: `a=1,b=2,c=3` },
-    { bin: 'npx', params: ['. -- "a=1,b=2,c=3"'], expected: `a=1,b=2,c=3` },
   ]
 
   // ensure that all tests are either run or skipped
