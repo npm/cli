7dceecb1b72d3e75f4b745dd642052dc8aaaeb14
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-16 10:27:53 -0400

    fix(powershell): don't use Invoke-Expression if `ExpectingInput`

--- bin/npx.ps1
+++ bin/npx.ps1
@@ -22,7 +22,7 @@ if (Test-Path $NPM_PREFIX_NPX_CLI_JS) {
   $NPX_CLI_JS=$NPM_PREFIX_NPX_CLI_JS
 }
 
-if ($MyInvocation.Line) { # used "-Command" argument
+if (-not $MyInvocation.ExpectingInput -and $MyInvocation.Line) { # used "-Command" argument
   if ($MyInvocation.Statement) {
     $NPX_ARGS = $MyInvocation.Statement.Substring($MyInvocation.InvocationName.Length).Trim()
   } else {
@@ -35,14 +35,7 @@ if ($MyInvocation.Line) { # used "-Command" argument
   $NODE_EXE = $NODE_EXE.Replace("``", "````")
   $NPX_CLI_JS = $NPX_CLI_JS.Replace("``", "````")
 
-  # Support pipeline input
-  if ($MyInvocation.ExpectingInput) {
-    $input = (@($input) -join "`n").Replace("``", "````")
-
-    Invoke-Expression "Write-Output `"$input`" | & `"$NODE_EXE`" `"$NPX_CLI_JS`" $NPX_ARGS"
-  } else {
-    Invoke-Expression "& `"$NODE_EXE`" `"$NPX_CLI_JS`" $NPX_ARGS"
-  }
+  Invoke-Expression "& `"$NODE_EXE`" `"$NPX_CLI_JS`" $NPX_ARGS"
 } else { # used "-File" argument
   # Support pipeline input
   if ($MyInvocation.ExpectingInput) {
