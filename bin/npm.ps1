be3cceb24b5ee2e58f57d04c5b634112cef55f82
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-23 10:02:23 -0400

    reduce new code added in half

--- bin/npm.ps1
+++ bin/npm.ps1
@@ -31,46 +31,16 @@ if ($MyInvocation.ExpectingInput) { # takes pipeline input
     $NPM_OG_COMMAND = $MyInvocation.Statement
   } else {
     $NPM_OG_COMMAND = (
-      [System.Management.Automation.InvocationInfo].GetProperty('ScriptPosition', [System.Reflection.BindingFlags] 'Instance, NonPublic')
+      [Management.Automation.InvocationInfo].GetProperty('ScriptPosition', [Reflection.BindingFlags] 'Instance, NonPublic')
     ).GetValue($MyInvocation).Text
   }
 
   $NODE_EXE = $NODE_EXE.Replace("``", "````")
   $NPM_CLI_JS = $NPM_CLI_JS.Replace("``", "````")
 
-  $ast = [System.Management.Automation.Language.Parser]::ParseInput($NPM_OG_COMMAND, [ref]$null, [ref]$null)
-  $redirections = $ast.FindAll({$args[0] -is [System.Management.Automation.Language.FileRedirectionAst]}, $true) | Sort-Object { $_.Extent.StartOffset }
-  $numberOfRedirects = @($redirections).Length
-
-  if ($numberOfRedirects -gt 0) {
-    $NPM_NO_REDIRECTS_COMMAND = ""
-    $prevEndOffset = 0
-    $i = 0
-
-    foreach ($redirection in $redirections) {
-      $parentExtentText = $redirection.Parent.Extent.Text
-      $startOffset = $redirection.Extent.StartOffset
-      $endOffset = $redirection.Extent.EndOffset
-
-      $changed = $parentExtentText.Substring($prevEndOffset, $startOffset - $prevEndOffset)
-      if ($i -eq $numberOfRedirects-1) {
-        if ($endOffset -eq $parentExtentText.Length) {
-          ### make valid powershell syntax when redirect on last line is removed
-          $changed += ";"
-        } else {
-          $changed += $parentExtentText.Substring($endOffset, $parentExtentText.Length - $endOffset)
-        }
-      }
-
-      $NPM_NO_REDIRECTS_COMMAND += $changed
-      $prevEndOffset = $endOffset
-      $i++
-    }
-
-    $NPM_ARGS = $NPM_NO_REDIRECTS_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
-  } else {
-    $NPM_ARGS = $NPM_OG_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
-  }
+  $NPM_NO_REDIRECTS_COMMAND = [Management.Automation.Language.Parser]::ParseInput($NPM_OG_COMMAND, [ref] $null, [ref] $null).
+    EndBlock.Statements.PipelineElements.CommandElements.Extent.Text -join ' '
+  $NPM_ARGS = $NPM_NO_REDIRECTS_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
 
   Invoke-Expression "& `"$NODE_EXE`" `"$NPM_CLI_JS`" $NPM_ARGS"
 }
--- bin/npx.ps1
+++ bin/npx.ps1
@@ -31,46 +31,16 @@ if ($MyInvocation.ExpectingInput) { # takes pipeline input
     $NPX_OG_COMMAND = $MyInvocation.Statement
   } else {
     $NPX_OG_COMMAND = (
-      [System.Management.Automation.InvocationInfo].GetProperty('ScriptPosition', [System.Reflection.BindingFlags] 'Instance, NonPublic')
+      [Management.Automation.InvocationInfo].GetProperty('ScriptPosition', [Reflection.BindingFlags] 'Instance, NonPublic')
     ).GetValue($MyInvocation).Text
   }
 
   $NODE_EXE = $NODE_EXE.Replace("``", "````")
   $NPX_CLI_JS = $NPX_CLI_JS.Replace("``", "````")
 
-  $ast = [System.Management.Automation.Language.Parser]::ParseInput($NPX_OG_COMMAND, [ref]$null, [ref]$null)
-  $redirections = $ast.FindAll({$args[0] -is [System.Management.Automation.Language.FileRedirectionAst]}, $true) | Sort-Object { $_.Extent.StartOffset }
-  $numberOfRedirects = @($redirections).Length
-
-  if ($numberOfRedirects -gt 0) {
-    $NPX_NO_REDIRECTS_COMMAND = ""
-    $prevEndOffset = 0
-    $i = 0
-
-    foreach ($redirection in $redirections) {
-      $parentExtentText = $redirection.Parent.Extent.Text
-      $startOffset = $redirection.Extent.StartOffset
-      $endOffset = $redirection.Extent.EndOffset
-
-      $changed = $parentExtentText.Substring($prevEndOffset, $startOffset - $prevEndOffset)
-      if ($i -eq $numberOfRedirects-1) {
-        if ($endOffset -eq $parentExtentText.Length) {
-          ### make valid powershell syntax when redirect on last line is removed
-          $changed += ";"
-        } else {
-          $changed += $parentExtentText.Substring($endOffset, $parentExtentText.Length - $endOffset)
-        }
-      }
-
-      $NPX_NO_REDIRECTS_COMMAND += $changed
-      $prevEndOffset = $endOffset
-      $i++
-    }
-
-    $NPX_ARGS = $NPX_NO_REDIRECTS_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
-  } else {
-    $NPX_ARGS = $NPX_OG_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
-  }
+  $NPX_NO_REDIRECTS_COMMAND = [Management.Automation.Language.Parser]::ParseInput($NPX_OG_COMMAND, [ref] $null, [ref] $null).
+    EndBlock.Statements.PipelineElements.CommandElements.Extent.Text -join ' '
+  $NPX_ARGS = $NPX_NO_REDIRECTS_COMMAND.Substring($MyInvocation.InvocationName.Length).Trim()
 
   Invoke-Expression "& `"$NODE_EXE`" `"$NPX_CLI_JS`" $NPX_ARGS"
 }
