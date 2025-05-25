1110958aedf46e5778047929498defc149c0d536
Marc Bernard <marc@marcbernardtools.com>
2025-05-25 14:48:05 +0200

    Move cursor blink logic to separate PR

--- test/lib/utils/display.js
+++ test/lib/utils/display.js
@@ -94,7 +94,7 @@ t.test('can do progress', async (t) => {
   log.error('', 'after input')
   output.standard('after input')
 
-  t.strictSame([...new Set(outputErrors)].sort(), ['\u001b[?25h', '\u001b[?25l', '-', '/', '\\', '|'])
+  t.strictSame([...new Set(outputErrors)].sort(), ['-', '/', '\\', '|'])
   t.strictSame(logs, ['error before input', 'error during input', 'error after input'])
   t.strictSame(outputs, ['before input', 'during input', 'after input'])
 })
