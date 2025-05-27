2a2651d090e3fcd5b3fc5ecf7b743b33b3766110
Alex Schwartz <alexschwartz01@gmail.com>
2025-05-27 06:48:17 -0400

    revert previous commits

--- workspaces/arborist/lib/arborist/reify.js
+++ workspaces/arborist/lib/arborist/reify.js
@@ -895,9 +895,7 @@ module.exports = cls => class Reifier extends cls {
         // Replace the host with the registry host while keeping the path intact
         resolvedURL.hostname = registryURL.hostname
         resolvedURL.port = registryURL.port
-        if (registryURL.protocol === 'https:' || registryURL.protocol === 'http:') {
-          resolvedURL.protocol = registryURL.protocol
-        }
+        resolvedURL.protocol = registryURL.protocol
 
         // Make sure we don't double-include the path if it's already there
         const registryPath = registryURL.pathname.replace(/\/$/, '')
