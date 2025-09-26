1e95c140a384e910860eedbd27a4b9b8e1ecbacd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-26 17:39:43 -0400

    chore: fix spelling: disabled
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/lib/commands/profile.js
+++ test/lib/commands/profile.js
@@ -968,7 +968,7 @@ t.test('disable-2fa', async t => {
 
     await profile.exec(['disable-2fa'])
     t.equal(result(), 'Two factor authentication not enabled.',
-      'should output already disalbed msg')
+      'should output already disabled msg')
   })
 
   t.test('requests otp', async t => {
@@ -1101,7 +1101,7 @@ t.test('disable-2fa', async t => {
 
     await profile.exec(['disable-2fa'])
 
-    t.equal(result(), 'Two factor authentication disabled.', 'should output already disalbed msg')
+    t.equal(result(), 'Two factor authentication disabled.', 'should output already disabled msg')
   })
 })
 
