30e375aca2a2727e40e5b2a7df39104b2707b91e
Michael Garvin <gar+gh@danger.computer>
2026-03-18 10:06:25 -0700

    chore: test snapshots

--- tap-snapshots/test/lib/docs.js.test.cjs
+++ tap-snapshots/test/lib/docs.js.test.cjs
@@ -1149,6 +1149,8 @@ of a relative number of days.
 
 This config cannot be used with: \`before\`
 
+This value is not exported to the environment for child processes.
+
 #### \`name\`
 
 * Default: null
