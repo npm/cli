dac568a634912a783fe1f8290893a28fc48ce5f5
lawrence3699 <lawrence3699@users.noreply.github.com>
2026-04-27 22:32:28 +1000

    fix(config): preserve min-release-age after flattening

--- workspaces/config/lib/definitions/definitions.js
+++ workspaces/config/lib/definitions/definitions.js
@@ -1378,7 +1378,6 @@ const definitions = {
       if (obj['min-release-age'] !== null) {
         flatOptions.before = new Date(Date.now() - (86400000 * obj['min-release-age']))
         obj.before = flatOptions.before
-        delete obj['min-release-age']
       }
     },
   }),
